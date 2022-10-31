b08f41a1034ddbf3998470761b79d0a2aafda7b4
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-10-30 20:41:23 -0700

    Create censorship.js

---
+++ tags/censorship.js
@@ -0,0 +1,20 @@
+function research(){
+    /* censorship 1 */
+
+    document.getElementById("cctitle1").innerHTML = 'Elon Musk buys Twitter.'
+    document.getElementById("cccontent1").innerHTML = ''
+    document.getElementById("cccontent01").innerHTML = ''
+    document.getElementById("cclink1").innerHTML = 'Read the Tweet'
+    document.getElementById("cclink1").href = 'https://twitter.com/elonmusk/status/1585341984679469056'
+    document.getElementById("cctime1").innerHTML = '10/30/22 8:36pm MST'
+
+    /* censorship 2 */
+
+    document.getElementById("cctitle11").innerHTML = 'YouTube issues hardstrike against StevenCrowder.'
+    document.getElementById("cccontent11").innerHTML = 'According to Crowder, YouTube did not provide specifics about why the strike was being issued other than that the video violated their harrassment and bullying guidelines.'
+    document.getElementById("cccontent011").innerHTML = ''
+    document.getElementById("cclink11").innerHTML = 'Read the post'
+    document.getElementById("cclink11").href = 'https://www.instagram.com/p/CkTu8-Iur9h/'
+    document.getElementById("cctime11").innerHTML = '10/30/22 8:40pm MST'
+
+}
