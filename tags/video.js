7b911e9101721b761a6aa10877fcfab674067165
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-10-30 20:31:13 -0700

    Update video.js

--- tags/video.js
+++ tags/video.js
@@ -2,8 +2,14 @@ function video(){
     /* Top most entry */
 
     document.getElementById("video").src = ''
+    /* rumble: rumble */
     document.getElementById("video").class = ''
+    /* rumble: 640 */
+    document.getElementById("video").width = ''
+    /* rumble: 360 */
+    document.getElementById("video").height = ''
     document.getElementById("videoalert").innerHTML = ''
-    
+
+
 }
 
