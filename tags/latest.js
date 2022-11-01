5c4dafb9c1823878021f5d3852519def637591c0
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-10-31 18:28:04 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -20,13 +20,13 @@ function latest(){
 
     /* Recent 2 */
 
-    document.getElementById("title11").innerHTML = 'Musk says that the Twitter board and Wachtell "deliberately hid this evidence from the court."'
-    document.getElementById("content11").innerHTML = '<blockquote class="twitter-tweet" data-dnt="true"><p lang="en" dir="ltr">"Wachtell &amp; Twitter board deliberately hid this evidence from the court. Stay tuned, more to come … "<a href="https://t.co/CifaNvtRtt">pic.twitter.com/CifaNvtRtt</a></p>&mdash; Elon Musk (@elonmusk) <a href="https://twitter.com/elonmusk/status/1586885887341645824?ref_src=twsrc%5Etfw">October 31, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> '
+    document.getElementById("title11").innerHTML = 'Donald J Trump requests SCOTUS to block the House Ways & Means Committee from gaining access to his tax returns.'
+    document.getElementById("content11").innerHTML = ''
     document.getElementById("content011").innerHTML = ''
-    document.getElementById("link11").innerHTML = 'Read the the Tweet'
-    document.getElementById("link11").href = 'https://twitter.com/elonmusk/status/1586885887341645824'
-    document.getElementById("time11").innerHTML = '10/30/22 6:23pm MST'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/40'
+    document.getElementById("link11").innerHTML = ''
+    document.getElementById("link11").href = ''
+    document.getElementById("time11").innerHTML = '10/31/22 6:27pm MST'
+    document.getElementById("telegram11").href = 'https://t.me/openteanews/41'
 
     document.getElementById("rlink11").innerHTML = ''
     document.getElementById("rlink11").href = ''
@@ -36,13 +36,13 @@ function latest(){
     
 
     /* Recent 3 */
-    document.getElementById("title111").innerHTML = 'Paul Pelosi is expected to make a full recovery following assault.'
-    document.getElementById("content111").innerHTML = 'Oct 28: The Pelosi home was invaded by the attacker who attacked Mr. Pelosi with a hammer.'
+    document.getElementById("title111").innerHTML = 'Musk says that the Twitter board and Wachtell "deliberately hid this evidence from the court."'
+    document.getElementById("content111").innerHTML = '<blockquote class="twitter-tweet" data-dnt="true"><p lang="en" dir="ltr">"Wachtell &amp; Twitter board deliberately hid this evidence from the court. Stay tuned, more to come … "<a href="https://t.co/CifaNvtRtt">pic.twitter.com/CifaNvtRtt</a></p>&mdash; Elon Musk (@elonmusk) <a href="https://twitter.com/elonmusk/status/1586885887341645824?ref_src=twsrc%5Etfw">October 31, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> '
     document.getElementById("content0111").innerHTML = ''
-    document.getElementById("link111").innerHTML = 'Read the Press Release'
-    document.getElementById("link111").href = 'https://www.speaker.gov/newsroom/102822-1'
-    document.getElementById("time111").innerHTML = '10/30/22 12:14pm MST'
-    document.getElementById("telegram111").href = 'https://t.me/openteanews/'
+    document.getElementById("link111").innerHTML = 'Read the the Tweet'
+    document.getElementById("link111").href = 'https://twitter.com/elonmusk/status/1586885887341645824'
+    document.getElementById("time111").innerHTML = '10/30/22 6:23pm MST'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/40'
 
     document.getElementById("rlink111").innerHTML = ''
     document.getElementById("rlink111").href = ''
@@ -55,32 +55,29 @@ function latest(){
 
      /* Recent 4 */
 
-     document.getElementById("title1111").innerHTML = 'FBI warns of scammers DMing social media users.'
-     document.getElementById("content1111").innerHTML = 'Scammers are utilizing social media services to DM (Direct Message) users in an attempt to get them to buy into fake cryptocurrency investments. <p>Ways to protect yourself:</p> <p>Do not accept DM requests from those you do not know</p> <p>Never share your crypto-wallet address with someone you do not know</p>'
+     document.getElementById("title1111").innerHTML = 'Paul Pelosi is expected to make a full recovery following assault.'
+     document.getElementById("content1111").innerHTML = 'Oct 28: The Pelosi home was invaded by the attacker who attacked Mr. Pelosi with a hammer.'
      document.getElementById("content01111").innerHTML = ''
-     document.getElementById("link1111").innerHTML = 'Read the Tweet'
-     document.getElementById("link1111").href = 'https://twitter.com/FBILasVegas/status/1586426436755267584'
-     document.getElementById("time1111").innerHTML = '10/29/22 8:39pm MST'
-     document.getElementById("telegram1111").href = 'https://t.me/openteanews/39'
- 
-     document.getElementById("rlink1111").innerHTML = 'Common cryptocurrency scams and how to avoid them - Kaspersky'
-     document.getElementById("rlink1111").href = 'https://www.kaspersky.com/resource-center/definitions/cryptocurrency-scams'
+     document.getElementById("link1111").innerHTML = 'Read the Press Release'
+     document.getElementById("link1111").href = 'https://www.speaker.gov/newsroom/102822-1'
+     document.getElementById("time1111").innerHTML = '10/30/22 12:14pm MST'
+     document.getElementById("telegram1111").href = 'https://t.me/openteanews/'
  
      document.getElementById("rlink01111").innerHTML = ''
      document.getElementById("rlink01111").href = ''
 
      /* Recent 5 */
 
-     document.getElementById("title11111").innerHTML = 'National Gas Price Average - 10/29/22'
-    document.getElementById("content11111").innerHTML = '$3.761'
-    document.getElementById("content011111").innerHTML = ''
-    document.getElementById("link11111").innerHTML = ''
-    document.getElementById("link11111").href = ''
-    document.getElementById("time11111").innerHTML = '10/29/22 4:40pm MST'
-    document.getElementById("telegram11111").href = 'https://t.me/openteanews/38'
-
-     document.getElementById("rlink11111").innerHTML = ''
-     document.getElementById("rlink11111").href = ''
+     document.getElementById("title11111").innerHTML = 'FBI warns of scammers DMing social media users.'
+     document.getElementById("content11111").innerHTML = 'Scammers are utilizing social media services to DM (Direct Message) users in an attempt to get them to buy into fake cryptocurrency investments. <p>Ways to protect yourself:</p> <p>Do not accept DM requests from those you do not know</p> <p>Never share your crypto-wallet address with someone you do not know</p>'
+     document.getElementById("content011111").innerHTML = ''
+     document.getElementById("link11111").innerHTML = 'Read the Tweet'
+     document.getElementById("link11111").href = 'https://twitter.com/FBILasVegas/status/1586426436755267584'
+     document.getElementById("time11111").innerHTML = '10/29/22 8:39pm MST'
+     document.getElementById("telegram11111").href = 'https://t.me/openteanews/39'
+ 
+     document.getElementById("rlink11111").innerHTML = 'Common cryptocurrency scams and how to avoid them - Kaspersky'
+     document.getElementById("rlink11111").href = 'https://www.kaspersky.com/resource-center/definitions/cryptocurrency-scams'
  
      document.getElementById("rlink011111").innerHTML = ''
      document.getElementById("rlink011111").href = ''
