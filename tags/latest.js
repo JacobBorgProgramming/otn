bc6844c9ba3ba7983092d7691b8ef9582347b2c0
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-01 18:00:34 -0700

    added BMC

--- tags/latest.js
+++ tags/latest.js
@@ -19,32 +19,32 @@ function latest(){
     /*https://tea.borgcreative.com/tags/telegram1.html */
 
     /* Recent 2 */
-    document.getElementById("title11").innerHTML = 'Facebook Content Request System'
-    document.getElementById("content11").innerHTML = 'Leaked documents show that Facebook has a special website for government officials to flag content on Facebook.'
-    document.getElementById("content011").innerHTML = ''
-    document.getElementById("link11").innerHTML = 'Read the document'
-    document.getElementById("link11").href = 'https://www.documentcloud.org/documents/23129270-fb-portal'
-    document.getElementById("time11").innerHTML = '10/31/22 12:04pm MST'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/'
+    document.getElementById("title11").innerHTML = 'Bill introduced would regulate major league sports teams abilities to relocate.'
+    document.getElementById("content11").innerHTML = 'If passed, H.R. 9151: SPURS Act, would introduce four requirements that would have to be met in order for a major sports team to relocate to a new community.'
+    document.getElementById("content011").innerHTML = 'The bill was introduced on October 7th, 2022 and has not been voted on at this time.'
+    document.getElementById("link11").innerHTML = 'Read the bill'
+    document.getElementById("link11").href = 'https://www.congress.gov/bill/117th-congress/house-bill/9151/text?q=%7B%22search%22%3A%5B%22H.R.+9151%3A+SPURS+Act%22%2C%22H.R.%22%2C%229151%3A%22%2C%22SPURS%22%2C%22Act%22%5D%7D&r=1&s=1'
+    document.getElementById("time11").innerHTML = '11/1/22 4:53pm MST'
+    document.getElementById("telegram11").href = 'https://t.me/openteanews/42'
 
-    document.getElementById("rlink11").innerHTML = 'Truth Cops: Leaked Documents Outline DHSs Plans to Police Disinformation - The Intercept'
-    document.getElementById("rlink11").href = 'https://theintercept.com/2022/10/31/social-media-disinformation-dhs/'
+    document.getElementById("rlink11").innerHTML = ''
+    document.getElementById("rlink11").href = ''
 
     document.getElementById("rlink011").innerHTML = ''
     document.getElementById("rlink011").href = ''
     
 
     /* Recent 3 */
-    document.getElementById("title111").innerHTML = 'Donald J Trump requests SCOTUS to block the House Ways & Means Committee from gaining access to his tax returns.'
-    document.getElementById("content111").innerHTML = ''
+    document.getElementById("title111").innerHTML = 'Facebook Content Request System'
+    document.getElementById("content111").innerHTML = 'Leaked documents show that Facebook has a special website for government officials to flag content on Facebook.'
     document.getElementById("content0111").innerHTML = ''
-    document.getElementById("link111").innerHTML = ''
-    document.getElementById("link111").href = ''
-    document.getElementById("time111").innerHTML = '10/31/22 6:27pm MST'
-    document.getElementById("telegram111").href = 'https://t.me/openteanews/41'
+    document.getElementById("link111").innerHTML = 'Read the document'
+    document.getElementById("link111").href = 'https://www.documentcloud.org/documents/23129270-fb-portal'
+    document.getElementById("time111").innerHTML = '10/31/22 12:04pm MST'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/'
 
-    document.getElementById("rlink111").innerHTML = ''
-    document.getElementById("rlink111").href = ''
+    document.getElementById("rlink111").innerHTML = 'Truth Cops: Leaked Documents Outline DHSs Plans to Police Disinformation - The Intercept'
+    document.getElementById("rlink111").href = 'https://theintercept.com/2022/10/31/social-media-disinformation-dhs/'
 
     document.getElementById("rlink0111").innerHTML = ''
     document.getElementById("rlink0111").href = ''
@@ -53,13 +53,13 @@ function latest(){
 
 
      /* Recent 4 */
-     document.getElementById("title1111").innerHTML = 'Musk says that the Twitter board and Wachtell "deliberately hid this evidence from the court."'
-     document.getElementById("content1111").innerHTML = '<blockquote class="twitter-tweet" data-dnt="true"><p lang="en" dir="ltr">"Wachtell &amp; Twitter board deliberately hid this evidence from the court. Stay tuned, more to come … "<a href="https://t.co/CifaNvtRtt">pic.twitter.com/CifaNvtRtt</a></p>&mdash; Elon Musk (@elonmusk) <a href="https://twitter.com/elonmusk/status/1586885887341645824?ref_src=twsrc%5Etfw">October 31, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> '
+     document.getElementById("title1111").innerHTML = 'Donald J Trump requests SCOTUS to block the House Ways & Means Committee from gaining access to his tax returns.'
+     document.getElementById("content1111").innerHTML = ''
      document.getElementById("content01111").innerHTML = ''
-     document.getElementById("link1111").innerHTML = 'Read the the Tweet'
-     document.getElementById("link1111").href = 'https://twitter.com/elonmusk/status/1586885887341645824'
-     document.getElementById("time1111").innerHTML = '10/30/22 6:23pm MST'
-     document.getElementById("telegram1111").href = 'https://t.me/openteanews/40'
+     document.getElementById("link1111").innerHTML = ''
+     document.getElementById("link1111").href = ''
+     document.getElementById("time1111").innerHTML = '10/31/22 6:27pm MST'
+     document.getElementById("telegram1111").href = 'https://t.me/openteanews/41'
  
      document.getElementById("rlink1111").innerHTML = ''
      document.getElementById("rlink1111").href = ''
@@ -69,13 +69,13 @@ function latest(){
 
      /* Recent 5 */
 
-     document.getElementById("title11111").innerHTML = 'Paul Pelosi is expected to make a full recovery following assault.'
-     document.getElementById("content11111").innerHTML = 'Oct 28: The Pelosi home was invaded by the attacker who attacked Mr. Pelosi with a hammer.'
+     document.getElementById("title11111").innerHTML = 'Musk says that the Twitter board and Wachtell "deliberately hid this evidence from the court."'
+     document.getElementById("content11111").innerHTML = '<blockquote class="twitter-tweet" data-dnt="true"><p lang="en" dir="ltr">"Wachtell &amp; Twitter board deliberately hid this evidence from the court. Stay tuned, more to come … "<a href="https://t.co/CifaNvtRtt">pic.twitter.com/CifaNvtRtt</a></p>&mdash; Elon Musk (@elonmusk) <a href="https://twitter.com/elonmusk/status/1586885887341645824?ref_src=twsrc%5Etfw">October 31, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> '
      document.getElementById("content011111").innerHTML = ''
-     document.getElementById("link11111").innerHTML = 'Read the Press Release'
-     document.getElementById("link11111").href = 'https://www.speaker.gov/newsroom/102822-1'
-     document.getElementById("time11111").innerHTML = '10/30/22 12:14pm MST'
-     document.getElementById("telegram11111").href = 'https://t.me/openteanews/'
+     document.getElementById("link11111").innerHTML = 'Read the the Tweet'
+     document.getElementById("link11111").href = 'https://twitter.com/elonmusk/status/1586885887341645824'
+     document.getElementById("time11111").innerHTML = '10/30/22 6:23pm MST'
+     document.getElementById("telegram11111").href = 'https://t.me/openteanews/40'
  
      document.getElementById("rlink11111").innerHTML = ''
      document.getElementById("rlink11111").href = ''
