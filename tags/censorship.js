b47df5982897c6a346fd7c21e9099bf36b62be53
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-07 13:44:51 -0700

    update

--- tags/censorship.js
+++ tags/censorship.js
@@ -1,12 +1,12 @@
 function censorship(){
     /* censorship 1 */
 
-    document.getElementById("cctitle1").innerHTML = 'Max Weinbach suspended from Instagram.'
-    document.getElementById("cccontent1").innerHTML = 'The 9to5Google contributor was reportedly suspended from Instagram without clear reasoning.'
+    document.getElementById("cctitle1").innerHTML = 'Kathy Griffin (@kathygriffin) suspended from Twitter for impersonation.'
+    document.getElementById("cccontent1").innerHTML = 'The comedian was suspened for impersonating Elon Musk.'
     document.getElementById("cccontent01").innerHTML = ''
-    document.getElementById("cclink1").innerHTML = 'Read the Tweet'
-    document.getElementById("cclink1").href = 'https://twitter.com/maxwinebach/status/1587087125110923267?s=46&t=dno5TCgYzFQnbkAHzH-4UQ'
-    document.getElementById("cctime1").innerHTML = '10/31/22 9:53am MST'
+    document.getElementById("cclink1").innerHTML = ''
+    document.getElementById("cclink1").href = ''
+    document.getElementById("cctime1").innerHTML = '11/7/22 1:40pm MST'
 
     /* censorship 2 */
 
