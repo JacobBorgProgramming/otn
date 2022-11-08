4eb7d4e288a87874d53f72513a45bbb042b69f05
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-08 08:24:47 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,5 +1,5 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = 'Midterm elections TOMORROW, November 8th.'
+    document.getElementById("acontent1").innerHTML = 'Midterm elections TODAY, November 8th.'
     document.getElementById("alink").innerHTML = ''
     document.getElementById("alink").href = ''
 }
