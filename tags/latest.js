9217ffaf44de8e49e88de31c41f16276f801ccb8
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-08 18:46:34 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,47 +1,47 @@
 function latest(){
     /* Top most entry */
-    document.getElementById("title1").innerHTML = 'Reports of tabulator malfunctions coming from Maricopa County, AZ voters.'
-    document.getElementById("content1").innerHTML = ''
+    document.getElementById("title1").innerHTML = 'Lawsuit filed in AZ against Maricopa County.'
+    document.getElementById("content1").innerHTML = 'According to the filing, at least 36% of Maricopa voting centers have experienced malfunctions with tabulators and/or printers.'
     document.getElementById("content01").innerHTML = ''
-    document.getElementById("link1").innerHTML = ''
-    document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '11/8/22 9:01am MST'
-    document.getElementById("telegram1").href = 'https://t.me/openteanews/49'
+    document.getElementById("link1").innerHTML = 'Read the filing'
+    document.getElementById("link1").href = 'https://prod-media-assets.protectthevote.com/media/document/news/AZ_Complaint_11.8_y8mj6e7n.pdf'
+    document.getElementById("time1").innerHTML = '11/8/22 6:46pm MST'
+    document.getElementById("telegram1").href = 'https://t.me/openteanews/50'
 
-    document.getElementById("rlink1").innerHTML = 'See Tyler Bowyers Tweet'
-    document.getElementById("rlink1").href = 'https://twitter.com/tylerbowyer/status/1589986706744578048'
+    document.getElementById("rlink1").innerHTML = 'Read the Tweet from Harmeet K. Dhillons '
+    document.getElementById("rlink1").href = 'https://twitter.com/pnjaban/status/1590130143309500416'
 
-    document.getElementById("rlink01").innerHTML = 'See The Post Millennials Tweet'
-    document.getElementById("rlink01").href = 'https://twitter.com/TPostMillennial/status/1589999456178831370?s=20&t=ZHsGIqvi7VQCyYHe5QrYUA'
+    document.getElementById("rlink01").innerHTML = ''
+    document.getElementById("rlink01").href = ''
 
     
     
     /*https://tea.borgcreative.com/tags/telegram1.html */
 
     /* Recent 2 */
-    document.getElementById("title11").innerHTML = 'Trump says that he will make a major announcement on November 15th.'
+    document.getElementById("title11").innerHTML = 'Reports of tabulator malfunctions coming from Maricopa County, AZ voters.'
     document.getElementById("content11").innerHTML = ''
     document.getElementById("content011").innerHTML = ''
     document.getElementById("link11").innerHTML = ''
     document.getElementById("link11").href = ''
-    document.getElementById("time11").innerHTML = '11/7/22 8:09pm MST'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/48'
+    document.getElementById("time11").innerHTML = '11/8/22 9:01am MST'
+    document.getElementById("telegram11").href = 'https://t.me/openteanews/49'
 
-    document.getElementById("rlink11").innerHTML = ''
-    document.getElementById("rlink11").href = ''
+    document.getElementById("rlink11").innerHTML = 'See Tyler Bowyers Tweet'
+    document.getElementById("rlink11").href = 'https://twitter.com/tylerbowyer/status/1589986706744578048'
 
-    document.getElementById("rlink011").innerHTML = ''
-    document.getElementById("rlink011").href = ''
-    
+    document.getElementById("rlink011").innerHTML = 'See The Post Millennials Tweet'
+    document.getElementById("rlink011").href = 'https://twitter.com/TPostMillennial/status/1589999456178831370?s=20&t=ZHsGIqvi7VQCyYHe5QrYUA'
+
 
     /* Recent 3 */
-    document.getElementById("title111").innerHTML = 'DOJ announces the largest US DOJ seizure of cryptocurreny: 50,676.17851897 BTC.'
-    document.getElementById("content111").innerHTML = 'At the time, the 50,676.17851897 BTC was valued at $3.36 billion.'
+    document.getElementById("title111").innerHTML = 'Trump says that he will make a major announcement on November 15th.'
+    document.getElementById("content111").innerHTML = ''
     document.getElementById("content0111").innerHTML = ''
-    document.getElementById("link111").innerHTML = 'Read the release'
-    document.getElementById("link111").href = 'https://www.justice.gov/usao-sdny/pr/us-attorney-announces-historic-336-billion-cryptocurrency-seizure-and-conviction'
-    document.getElementById("time111").innerHTML = '11/7/22 2:01pm MST'
-    document.getElementById("telegram111").href = 'https://t.me/openteanews/46'
+    document.getElementById("link111").innerHTML = ''
+    document.getElementById("link111").href = ''
+    document.getElementById("time111").innerHTML = '11/7/22 8:09pm MST'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/48'
 
     document.getElementById("rlink111").innerHTML = ''
     document.getElementById("rlink111").href = ''
@@ -53,13 +53,13 @@ function latest(){
 
 
      /* Recent 4 */
-     document.getElementById("title1111").innerHTML = 'Various corporations suspend advertising on Twitter in response to Elon Musk purchasing the social network.'
-     document.getElementById("content1111").innerHTML = ''
+     document.getElementById("title1111").innerHTML = 'DOJ announces the largest US DOJ seizure of cryptocurreny: 50,676.17851897 BTC.'
+     document.getElementById("content1111").innerHTML = 'At the time, the 50,676.17851897 BTC was valued at $3.36 billion.'
      document.getElementById("content01111").innerHTML = ''
-     document.getElementById("link1111").innerHTML = ''
-     document.getElementById("link1111").href = ''
-     document.getElementById("time1111").innerHTML = '11/7/22 1:44pm MST'
-     document.getElementById("telegram1111").href = 'https://t.me/openteanews/45'
+     document.getElementById("link1111").innerHTML = 'Read the release'
+     document.getElementById("link1111").href = 'https://www.justice.gov/usao-sdny/pr/us-attorney-announces-historic-336-billion-cryptocurrency-seizure-and-conviction'
+     document.getElementById("time1111").innerHTML = '11/7/22 2:01pm MST'
+     document.getElementById("telegram1111").href = 'https://t.me/openteanews/46'
  
      document.getElementById("rlink1111").innerHTML = ''
      document.getElementById("rlink1111").href = ''
@@ -69,13 +69,13 @@ function latest(){
 
      /* Recent 5 */
 
-     document.getElementById("title11111").innerHTML = 'POTUS draws line between the attack on Paul Pelosi and Jan 6 in speech (11/2).'
-     document.getElementById("content11111").innerHTML = '"...after the assailant entered the home asking, “Wheres Nancy?  Wheres Nancy?” Those were the very same words used by the mob when they stormed the United States Capitol on January the 6th..."'
-     document.getElementById("content011111").innerHTML = '"...It was an enraged mob that had been whipped up into a frenzy by a President repeating over and over again the Big Lie that the election of 2020 had been stolen."'
-     document.getElementById("link11111").innerHTML = 'Read the full speech'
-     document.getElementById("link11111").href = 'https://www.whitehouse.gov/briefing-room/speeches-remarks/2022/11/03/remarks-by-president-biden-on-standing-up-for-democracy/'
-     document.getElementById("time11111").innerHTML = '11/3/22 4:53pm MST'
-     document.getElementById("telegram11111").href = 'https://t.me/openteanews/44'
+     document.getElementById("title11111").innerHTML = 'Various corporations suspend advertising on Twitter in response to Elon Musk purchasing the social network.'
+     document.getElementById("content11111").innerHTML = ''
+     document.getElementById("content011111").innerHTML = ''
+     document.getElementById("link11111").innerHTML = ''
+     document.getElementById("link11111").href = ''
+     document.getElementById("time11111").innerHTML = '11/7/22 1:44pm MST'
+     document.getElementById("telegram11111").href = 'https://t.me/openteanews/45'
  
      document.getElementById("rlink1111").innerHTML = ''
      document.getElementById("rlink1111").href = ''
