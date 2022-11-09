9f010eee71476aaca48d4caa16a14d59625b29f2
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-08 19:01:44 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,6 +1,6 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = 'Midterm elections TODAY, November 8th.'
-    document.getElementById("alink").innerHTML = ''
-    document.getElementById("alink").href = ''
+    document.getElementById("acontent1").innerHTML = 'Midterm elections TODAY, November 8th. '
+    document.getElementById("alink").innerHTML = 'See the latest results.'
+    document.getElementById("alink").href = 'https://www.politico.com/2022-election/results/'
 }
 
