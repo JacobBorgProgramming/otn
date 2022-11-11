75c3d4d4410c7a901c14c68e3a8bef74a09eb04a
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-11 13:44:01 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,6 +1,6 @@
 function alert(){
     document.getElementById("acontent1").innerHTML = ''
     document.getElementById("alink").innerHTML = '2022 Midterm Election Latest Results'
-    document.getElementById("alink").href = 'https://www.politico.com/2022-election/results/'
+    document.getElementById("alink").href = 'https://www.realclearpolitics.com/'
 }
 
