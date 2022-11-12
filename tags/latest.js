d67d373f08ae8d3d831bbcc0cd142e3f795305cf
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-12 13:09:12 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,15 +1,15 @@
 function latest(){
     /* Top most entry */
-    document.getElementById("title1").innerHTML = 'FTX Group files voluntary chapter 11'
-    document.getElementById("content1").innerHTML = 'FTX is a digital asset exchange service.'
-    document.getElementById("content01").innerHTML = 'The CEO, Sam Bankman-Fried (SBF) has resigned from his position as CEO.'
-    document.getElementById("link1").innerHTML = 'Read the Tweet from the FTX CEO'
-    document.getElementById("link1").href = 'https://twitter.com/SBF_FTX/status/1591089317300293636'
-    document.getElementById("time1").innerHTML = '11/11/22 3:11pm MST'
-    document.getElementById("telegram1").href = 'https://t.me/openteanews/52'
+    document.getElementById("title1").innerHTML = 'DailyMail: FOUR HUNDRED private jets arrived in Egypt during COP27 as climate delegates are accused of "hypocrisy"'
+    document.getElementById("content1").innerHTML = ''
+    document.getElementById("content01").innerHTML = ''
+    document.getElementById("link1").innerHTML = 'Read the article'
+    document.getElementById("link1").href = 'https://www.dailymail.co.uk/news/article-11416209/FOUR-private-jets-arrived-Egypt-COP27-climate-delegates-accused-hypocrisy.html'
+    document.getElementById("time1").innerHTML = '11/12/22 1:08pm MST'
+    document.getElementById("telegram1").href = 'https://t.me/openteanews/53'
 
-    document.getElementById("rlink1").innerHTML = 'The release from FTX Group'
-    document.getElementById("rlink1").href = 'https://twitter.com/FTX_Official/status/1591071832823959552/photo/1'
+    document.getElementById("rlink1").innerHTML = ''
+    document.getElementById("rlink1").href = ''
 
     document.getElementById("rlink01").innerHTML = ''
     document.getElementById("rlink01").href = ''
@@ -19,32 +19,32 @@ function latest(){
     /*https://tea.borgcreative.com/tags/telegram1.html */
 
     /* Recent 2 */
-    document.getElementById("title11").innerHTML = 'David DePape has been indicted.'
-    document.getElementById("content11").innerHTML = 'DePape has been charged with "one count of assault upon an immediate family member of a U.S. official with the intent to retaliate against the official on account of the performance of official duties"'
-    document.getElementById("content011").innerHTML = 'and "one count of attempted kidnapping of a U.S. official on account of the performance of official duties."'
-    document.getElementById("link11").innerHTML = 'Read the DOJ Release'
-    document.getElementById("link11").href = 'https://www.justice.gov/opa/pr/man-indicted-connection-attempted-kidnapping-and-assault-pelosi-residence'
-    document.getElementById("time11").innerHTML = '11/10/22 3:37pm MST'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/51'
+    document.getElementById("title11").innerHTML = 'FTX Group files voluntary chapter 11'
+    document.getElementById("content11").innerHTML = 'FTX is a digital asset exchange service.'
+    document.getElementById("content011").innerHTML = 'The CEO, Sam Bankman-Fried (SBF) has resigned from his position as CEO.'
+    document.getElementById("link11").innerHTML = 'Read the Tweet from the FTX CEO'
+    document.getElementById("link11").href = 'https://twitter.com/SBF_FTX/status/1591089317300293636'
+    document.getElementById("time11").innerHTML = '11/11/22 3:11pm MST'
+    document.getElementById("telegram11").href = 'https://t.me/openteanews/52'
 
-    document.getElementById("rlink11").innerHTML = ''
-    document.getElementById("rlink11").href = ''
+    document.getElementById("rlink11").innerHTML = 'The release from FTX Group'
+    document.getElementById("rlink11").href = 'https://twitter.com/FTX_Official/status/1591071832823959552/photo/1'
 
     document.getElementById("rlink011").innerHTML = ''
     document.getElementById("rlink011").href = ''
 
 
     /* Recent 3 */
-    document.getElementById("title111").innerHTML = 'Republicans file lawsuit in AZ against Maricopa County.'
-    document.getElementById("content111").innerHTML = 'LATEST: Judge rules against the TRO request.'
-    document.getElementById("content0111").innerHTML = '<p>- - -</p><p>According to the filing, at least 36% of Maricopa voting centers have experienced malfunctions with tabulators and/or printers.</p> <p>The suit seeks to extend voting hours until 10pm, November 8th, 2022. </p>'
-    document.getElementById("link111").innerHTML = 'Read the filing'
-    document.getElementById("link111").href = 'https://prod-media-assets.protectthevote.com/media/document/news/AZ_Complaint_11.8_y8mj6e7n.pdf'
-    document.getElementById("time111").innerHTML = '<p>Updated 11/8/22 7:33pm MST</p><p>11/8/22 6:46pm MST</p>'
-    document.getElementById("telegram111").href = 'https://t.me/openteanews/50'
+    document.getElementById("title111").innerHTML = 'David DePape has been indicted.'
+    document.getElementById("content111").innerHTML = 'DePape has been charged with "one count of assault upon an immediate family member of a U.S. official with the intent to retaliate against the official on account of the performance of official duties"'
+    document.getElementById("content0111").innerHTML = 'and "one count of attempted kidnapping of a U.S. official on account of the performance of official duties."'
+    document.getElementById("link111").innerHTML = 'Read the DOJ Release'
+    document.getElementById("link111").href = 'https://www.justice.gov/opa/pr/man-indicted-connection-attempted-kidnapping-and-assault-pelosi-residence'
+    document.getElementById("time111").innerHTML = '11/10/22 3:37pm MST'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/51'
 
-    document.getElementById("rlink111").innerHTML = 'Read the Tweet from Harmeet K. Dhillon'
-    document.getElementById("rlink111").href = 'https://twitter.com/pnjaban/status/1590130143309500416'
+    document.getElementById("rlink111").innerHTML = ''
+    document.getElementById("rlink111").href = ''
 
     document.getElementById("rlink0111").innerHTML = ''
     document.getElementById("rlink0111").href = ''
@@ -53,36 +53,36 @@ function latest(){
 
 
      /* Recent 4 */
-     document.getElementById("title1111").innerHTML = 'Reports of tabulator malfunctions coming from Maricopa County, AZ voters.'
-     document.getElementById("content1111").innerHTML = ''
-     document.getElementById("content01111").innerHTML = ''
-     document.getElementById("link1111").innerHTML = ''
-     document.getElementById("link1111").href = ''
-     document.getElementById("time1111").innerHTML = '11/8/22 9:01am MST'
-     document.getElementById("telegram1111").href = 'https://t.me/openteanews/49'
+     document.getElementById("title1111").innerHTML = 'Republicans file lawsuit in AZ against Maricopa County.'
+     document.getElementById("content1111").innerHTML = 'LATEST: Judge rules against the TRO request.'
+     document.getElementById("content01111").innerHTML = '<p>- - -</p><p>According to the filing, at least 36% of Maricopa voting centers have experienced malfunctions with tabulators and/or printers.</p> <p>The suit seeks to extend voting hours until 10pm, November 8th, 2022. </p>'
+     document.getElementById("link1111").innerHTML = 'Read the filing'
+     document.getElementById("link1111").href = 'https://prod-media-assets.protectthevote.com/media/document/news/AZ_Complaint_11.8_y8mj6e7n.pdf'
+     document.getElementById("time1111").innerHTML = '<p>Updated 11/8/22 7:33pm MST</p><p>11/8/22 6:46pm MST</p>'
+     document.getElementById("telegram1111").href = 'https://t.me/openteanews/50'
  
-     document.getElementById("rlink1111").innerHTML = 'See Tyler Bowyers Tweet'
-     document.getElementById("rlink1111").href = 'https://twitter.com/tylerbowyer/status/1589986706744578048'
+     document.getElementById("rlink1111").innerHTML = 'Read the Tweet from Harmeet K. Dhillon'
+     document.getElementById("rlink1111").href = 'https://twitter.com/pnjaban/status/1590130143309500416'
  
-     document.getElementById("rlink01111").innerHTML = 'See The Post Millennials Tweet'
-     document.getElementById("rlink01111").href = 'https://twitter.com/TPostMillennial/status/1589999456178831370?s=20&t=ZHsGIqvi7VQCyYHe5QrYUA'
+     document.getElementById("rlink01111").innerHTML = ''
+     document.getElementById("rlink01111").href = ''
  
      /* Recent 5 */
 
-     document.getElementById("title11111").innerHTML = 'Trump says that he will make a major announcement on November 15th.'
+     document.getElementById("title11111").innerHTML = 'Reports of tabulator malfunctions coming from Maricopa County, AZ voters.'
      document.getElementById("content11111").innerHTML = ''
      document.getElementById("content011111").innerHTML = ''
      document.getElementById("link11111").innerHTML = ''
      document.getElementById("link11111").href = ''
-     document.getElementById("time11111").innerHTML = '11/7/22 8:09pm MST'
-     document.getElementById("telegram11111").href = 'https://t.me/openteanews/48'
+     document.getElementById("time11111").innerHTML = '11/8/22 9:01am MST'
+     document.getElementById("telegram11111").href = 'https://t.me/openteanews/49'
  
-     document.getElementById("rlink1111").innerHTML = ''
-     document.getElementById("rlink1111").href = ''
+     document.getElementById("rlink11111").innerHTML = 'See Tyler Bowyers Tweet'
+     document.getElementById("rlink11111").href = 'https://twitter.com/tylerbowyer/status/1589986706744578048'
  
-     document.getElementById("rlink011111").innerHTML = ''
-     document.getElementById("rlink011111").href = ''
-
+     document.getElementById("rlink011111").innerHTML = 'See The Post Millennials Tweet'
+     document.getElementById("rlink011111").href = 'https://twitter.com/TPostMillennial/status/1589999456178831370?s=20&t=ZHsGIqvi7VQCyYHe5QrYUA'
+ 
  
  
 
