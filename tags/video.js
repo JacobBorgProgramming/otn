baf3d41fb037afdf0eda90d65fed6c80c50d6917
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-13 16:28:00 -0700

    updates

--- tags/video.js
+++ tags/video.js
@@ -5,9 +5,9 @@ function video(){
     /* rumble: rumble */
     document.getElementById("video").class = ''
     /* rumble: 640 */
-    document.getElementById("video").width = ''
+    document.getElementById("video").width = '0px'
     /* rumble: 360 */
-    document.getElementById("video").height = ''
+    document.getElementById("video").height = '0px'
 
     document.getElementById("videoalert").innerHTML = 'The are no broadcasts at this time.'
 
