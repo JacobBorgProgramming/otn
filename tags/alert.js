dcd1de628908551b6f06ff4896a1d5ade53d5eea
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-13 15:55:55 -0700

    updates
    -Alert improvements
    -Alert box color can now be edited from tag manager

--- tags/alert.js
+++ tags/alert.js
@@ -1,6 +1,13 @@
 function alert(){
+    document.getElementById("feed-alert").class = 'alert alert-'
     document.getElementById("acontent1").innerHTML = ''
     document.getElementById("alink").innerHTML = '2022 Midterm Election Latest Results'
     document.getElementById("alink").href = 'https://www.realclearpolitics.com/'
 }
 
+/*
+    warning = yellow
+    danger = red
+    success = green
+    secondary = grey
+*/
