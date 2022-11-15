3c06bd8e53fd31660557e3d1bfc518b92c676157
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-15 16:53:27 -0700

    Update video.js

--- tags/video.js
+++ tags/video.js
@@ -1,15 +1,15 @@
 function video(){
     /* Top most entry */
 
-    document.getElementById("video").src = ''
+    document.getElementById("video").src = 'https://rumble.com/embed/v1rrnvq/?pub=3cqil'
     /* rumble: rumble */
-    document.getElementById("video").class = ''
+    document.getElementById("video").class = 'rumble'
     /* rumble: 640 */
-    document.getElementById("video").width = '0px'
+    document.getElementById("video").width = '640px'
     /* rumble: 360 */
-    document.getElementById("video").height = '0px'
+    document.getElementById("video").height = '360px'
 
-    document.getElementById("videoalert").innerHTML = 'The are no broadcasts at this time.'
+    document.getElementById("videoalert").innerHTML = ''
 
 
 }
