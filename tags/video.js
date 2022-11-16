1a80b8b6f60259e71fcecbba7e8011e472fb905c
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-15 17:06:29 -0700

    Update video.js

--- tags/video.js
+++ tags/video.js
@@ -9,7 +9,7 @@ function video(){
     /* rumble: 360 */
     document.getElementById("video").height = ''
 
-    document.getElementById("videoalert").innerHTML = '<script>!function(r,u,m,b,l,e){r._Rumble=b,r[b]||(r[b]=function(){(r[b]._=r[b]._||[]).push(arguments);if(r[b]._.length==1){l=u.createElement(m),e=u.getElementsByTagName(m)[0],l.async=1,l.src="https://rumble.com/embedJS/u3cqil"+(arguments[1].video?'.'+arguments[1].video:'')+"/?url="+encodeURIComponent(location.href)+"&args="+encodeURIComponent(JSON.stringify([].slice.apply(arguments))),e.parentNode.insertBefore(l,e)}})}(window, document, "script", "Rumble");</script><div id="rumble_v1rrnvq"></div><script>Rumble("play", {"video":"v1rrnvq","div":"rumble_v1rrnvq"});</script>'
+    document.getElementById("videoalert").innerHTML = '<script>!function(r,u,m,b,l,e){r._Rumble=b,r[b]||(r[b]=function(){(r[b]._=r[b]._||[]).push(arguments);if(r[b]._.length==1){l=u.createElement(m),e=u.getElementsByTagName(m)[0],l.async=1,l.src="https://rumble.com/embedJS/u3cqil"+(arguments[1].video?'.'+arguments[1].video:'')+"/?url="+encodeURIComponent(location.href)+"&args="+encodeURIComponent(JSON.stringify([].slice.apply(arguments))),e.parentNode.insertBefore(l,e)}})}(window, document, "script", "Rumble");</script><div id="rumble_v1rrnvq"></div><script>Rumble("play", {"video":"v1rrnvq","div":"rumble_v1rrnvq"});</script><p>"Join President Donald J. Trump, 45th President of the United States of America, for a special announcement at the Mar-a-Lago Club in Palm Beach, Florida on Tuesday, November 15, 2022, at 9:00PM EST."</p>'
 
 
 }
