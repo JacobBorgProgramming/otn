1de3967a1980cf674c09ea717c91d879f3845a8f
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-15 17:02:32 -0700

    Update video.js

--- tags/video.js
+++ tags/video.js
@@ -1,15 +1,15 @@
 function video(){
     /* Top most entry */
 
-    document.getElementById("video").src = 'https://rumble.com/embed/v1rrnvq/?pub=3cqil'
+    document.getElementById("video").src = ''
     /* rumble: rumble */
-    document.getElementById("video").class = 'rumble'
+    document.getElementById("video").class = ''
     /* rumble: 640 */
-    document.getElementById("video").width = '640'
+    document.getElementById("video").width = ''
     /* rumble: 360 */
-    document.getElementById("video").height = '360'
+    document.getElementById("video").height = ''
 
-    document.getElementById("videoalert").innerHTML = ''
+    document.getElementById("videoalert").innerHTML = '<script>!function(r,u,m,b,l,e){r._Rumble=b,r[b]||(r[b]=function(){(r[b]._=r[b]._||[]).push(arguments);if(r[b]._.length==1){l=u.createElement(m),e=u.getElementsByTagName(m)[0],l.async=1,l.src="https://rumble.com/embedJS/u3cqil"+(arguments[1].video?'.'+arguments[1].video:'')+"/?url="+encodeURIComponent(location.href)+"&args="+encodeURIComponent(JSON.stringify([].slice.apply(arguments))),e.parentNode.insertBefore(l,e)}})}(window, document, "script", "Rumble");</script><div id="rumble_v1rrnvq"></div><script>Rumble("play", {"video":"v1rrnvq","div":"rumble_v1rrnvq"});</script>'
 
 
 }
