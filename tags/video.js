bc708cb629bfccb8fcb1525d3428f94a3298c13d
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-15 20:11:36 -0700

    Update video.js

--- tags/video.js
+++ tags/video.js
@@ -9,8 +9,11 @@ function video(){
     /* rumble: 360 */
     document.getElementById("video").height = ''
 
-    document.getElementById("videoalert").innerHTML = '<script>!function(r,u,m,b,l,e){r._Rumble=b,r[b]||(r[b]=function(){(r[b]._=r[b]._||[]).push(arguments);if(r[b]._.length==1){l=u.createElement(m),e=u.getElementsByTagName(m)[0],l.async=1,l.src="https://rumble.com/embedJS/u3cqil"+(arguments[1].video?'.'+arguments[1].video:'')+"/?url="+encodeURIComponent(location.href)+"&args="+encodeURIComponent(JSON.stringify([].slice.apply(arguments))),e.parentNode.insertBefore(l,e)}})}(window, document, "script", "Rumble");</script><div id="rumble_v1rrnvq"></div><script>Rumble("play", {"video":"v1rrnvq","div":"rumble_v1rrnvq"});</script>'
-
+    document.getElementById("videoalert").innerHTML = 'There are no broadcasts at this time.'
+/*
+There are no broadcasts at this time.
+Tune in at 00:00 for 
+*/
 
 }
 
