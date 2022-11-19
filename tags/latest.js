f10c5556309e2fc3588c6bcbc84ecbfbb7328fb7
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-19 13:22:05 -0700

    updates

--- tags/latest.js
+++ tags/latest.js
@@ -1,12 +1,12 @@
 function latest(){
     /* Top most entry */
-    document.getElementById("title1").innerHTML = 'California made a $192,000,000.00 deal for a "COVID19 Graphic Designer."'
-    document.getElementById("content1").innerHTML = ''
+    document.getElementById("title1").innerHTML = 'CBS News is suspending its use of Twitter, per Forbes.'
+    document.getElementById("content1").innerHTML = 'The decision comes not long after Elon Musk took over the social networking site.'
     document.getElementById("content01").innerHTML = ''
-    document.getElementById("link1").innerHTML = 'Read the Tweet'
-    document.getElementById("link1").href = 'https://twitter.com/jborg_arts/status/1592983294974386176'
-    document.getElementById("time1").innerHTML = '11/17/22 11:00am MST'
-    document.getElementById("telegram1").href = 'https://t.me/openteanews/55'
+    document.getElementById("link1").innerHTML = 'Read the article from Forbes'
+    document.getElementById("link1").href = 'https://www.forbes.com/sites/markjoyella/2022/11/19/cbs-news-suspends-all-activity-on-twitter-in-light-of-the-uncertainty-surrounding-platform-under-musk/?sh=38c8d66fff96'
+    document.getElementById("time1").innerHTML = '11/19/22 1:21pm MST'
+    document.getElementById("telegram1").href = 'https://t.me/openteanews/56'
 
     document.getElementById("rlink1").innerHTML = ''
     document.getElementById("rlink1").href = ''
@@ -19,13 +19,13 @@ function latest(){
     /*https://tea.borgcreative.com/tags/telegram1.html */
 
     /* Recent 2 */
-    document.getElementById("title11").innerHTML = 'Per the latest update from CA SOS (11/10/22), there are still 4,829,879 unprocessed ballots in California.'
+    document.getElementById("title11").innerHTML = 'California made a $192,000,000.00 deal for a "COVID19 Graphic Designer."'
     document.getElementById("content11").innerHTML = ''
     document.getElementById("content011").innerHTML = ''
-    document.getElementById("link11").innerHTML = 'Read the Document'
-    document.getElementById("link11").href = 'https://elections.cdn.sos.ca.gov/statewide-elections/2022-general/unprocessed-ballots-report.pdf'
-    document.getElementById("time11").innerHTML = '11/13/22 2:53pm MST'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/54'
+    document.getElementById("link11").innerHTML = 'Read the Tweet'
+    document.getElementById("link11").href = 'https://twitter.com/jborg_arts/status/1592983294974386176'
+    document.getElementById("time11").innerHTML = '11/17/22 11:00am MST'
+    document.getElementById("telegram11").href = 'https://t.me/openteanews/55'
 
     document.getElementById("rlink11").innerHTML = ''
     document.getElementById("rlink11").href = ''
@@ -35,13 +35,13 @@ function latest(){
 
 
     /* Recent 3 */
-    document.getElementById("title111").innerHTML = 'We looked at 4 California counties to see if the common talking point “Democratic areas have better education than Republican” is true. '
+    document.getElementById("title111").innerHTML = 'Per the latest update from CA SOS (11/10/22), there are still 4,829,879 unprocessed ballots in California.'
     document.getElementById("content111").innerHTML = ''
     document.getElementById("content0111").innerHTML = ''
-    document.getElementById("link111").innerHTML = 'Here are the results:'
-    document.getElementById("link111").href = 'https://www.openteanews.com/data/education/CaliforniaSchools4countyReview.html'
-    document.getElementById("time111").innerHTML = '11/13/22 2:52pm MST'
-    document.getElementById("telegram111").href = 'https://t.me/openteanews/'
+    document.getElementById("link111").innerHTML = 'Read the Document'
+    document.getElementById("link111").href = 'https://elections.cdn.sos.ca.gov/statewide-elections/2022-general/unprocessed-ballots-report.pdf'
+    document.getElementById("time111").innerHTML = '11/13/22 2:53pm MST'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/54'
 
     document.getElementById("rlink111").innerHTML = ''
     document.getElementById("rlink111").href = ''
@@ -53,13 +53,13 @@ function latest(){
 
 
      /* Recent 4 */
-     document.getElementById("title1111").innerHTML = 'DailyMail: FOUR HUNDRED private jets arrived in Egypt during COP27 as climate delegates are accused of "hypocrisy"'
+     document.getElementById("title1111").innerHTML = 'We looked at 4 California counties to see if the common talking point “Democratic areas have better education than Republican” is true. '
      document.getElementById("content1111").innerHTML = ''
      document.getElementById("content01111").innerHTML = ''
-     document.getElementById("link1111").innerHTML = 'Read the article'
-     document.getElementById("link1111").href = 'https://www.dailymail.co.uk/news/article-11416209/FOUR-private-jets-arrived-Egypt-COP27-climate-delegates-accused-hypocrisy.html'
-     document.getElementById("time1111").innerHTML = '11/12/22 1:08pm MST'
-     document.getElementById("telegram1111").href = 'https://t.me/openteanews/53'
+     document.getElementById("link1111").innerHTML = 'Here are the results:'
+     document.getElementById("link1111").href = 'https://www.openteanews.com/data/education/CaliforniaSchools4countyReview.html'
+     document.getElementById("time1111").innerHTML = '11/13/22 2:52pm MST'
+     document.getElementById("telegram1111").href = 'https://t.me/openteanews/'
  
      document.getElementById("rlink1111").innerHTML = ''
      document.getElementById("rlink1111").href = ''
@@ -69,16 +69,16 @@ function latest(){
  
      /* Recent 5 */
 
-     document.getElementById("title11111").innerHTML = 'FTX Group files voluntary chapter 11'
-     document.getElementById("content11111").innerHTML = 'FTX is a digital asset exchange service.'
-     document.getElementById("content011111").innerHTML = 'The CEO, Sam Bankman-Fried (SBF) has resigned from his position as CEO.'
-     document.getElementById("link11111").innerHTML = 'Read the Tweet from the FTX CEO'
-     document.getElementById("link11111").href = 'https://twitter.com/SBF_FTX/status/1591089317300293636'
-     document.getElementById("time11111").innerHTML = '11/11/22 3:11pm MST'
-     document.getElementById("telegram11111").href = 'https://t.me/openteanews/52'
+     document.getElementById("title11111").innerHTML = 'DailyMail: FOUR HUNDRED private jets arrived in Egypt during COP27 as climate delegates are accused of "hypocrisy"'
+     document.getElementById("content11111").innerHTML = ''
+     document.getElementById("content011111").innerHTML = ''
+     document.getElementById("link11111").innerHTML = 'Read the article'
+     document.getElementById("link11111").href = 'https://www.dailymail.co.uk/news/article-11416209/FOUR-private-jets-arrived-Egypt-COP27-climate-delegates-accused-hypocrisy.html'
+     document.getElementById("time11111").innerHTML = '11/12/22 1:08pm MST'
+     document.getElementById("telegram11111").href = 'https://t.me/openteanews/53'
  
-     document.getElementById("rlink11111").innerHTML = 'The release from FTX Group'
-     document.getElementById("rlink11111").href = 'https://twitter.com/FTX_Official/status/1591071832823959552/photo/1'
+     document.getElementById("rlink11111").innerHTML = ''
+     document.getElementById("rlink11111").href = ''
  
      document.getElementById("rlink011111").innerHTML = ''
      document.getElementById("rlink011111").href = ''
