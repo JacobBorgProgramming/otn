9248c530fcc5eb0d446b445246313b49207c004a
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-26 10:30:48 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -2,87 +2,90 @@ function latest(){
     /* Top most entry */
     var l5 = "1"
     document.getElementById("title"+l5).innerHTML = 'Elon Musks "General Amnesty" Twitter poll has ended.'
-    document.getElementById("content1").innerHTML = 'Yes: 72.4%'
-    document.getElementById("content01").innerHTML = 'No: 27.6%'
-    document.getElementById("link1").innerHTML = 'See the Tweet'
-    document.getElementById("link1").href = 'https://twitter.com/elonmusk/status/1595473875847942146?s=20&t=2G_YriM2tq9egnXwh6cY_w'
-    document.getElementById("time1").innerHTML = '11/24/22 11:12am MST'
-    document.getElementById("telegram1").href = 'https://t.me/openteanews/60'
+    document.getElementById("content"+l5).innerHTML = 'Yes: 72.4%'
+    document.getElementById("content0"+l5).innerHTML = 'No: 27.6%'
+    document.getElementById("link"+l5).innerHTML = 'See the Tweet'
+    document.getElementById("link"+l5).href = 'https://twitter.com/elonmusk/status/1595473875847942146?s=20&t=2G_YriM2tq9egnXwh6cY_w'
+    document.getElementById("time"+l5).innerHTML = '11/24/22 11:12am MST'
+    document.getElementById("telegram"+l5).href = 'https://t.me/openteanews/60'
 
-    document.getElementById("rlink1").innerHTML = ''
-    document.getElementById("rlink1").href = ''
+    document.getElementById("rlink"+l5).innerHTML = ''
+    document.getElementById("rlink"+l5).href = ''
 
-    document.getElementById("rlink01").innerHTML = ''
-    document.getElementById("rlink01").href = ''
+    document.getElementById("rlink0"+l5).innerHTML = ''
+    document.getElementById("rlink0"+l5).href = ''
 
     
     
     /*https://tea.borgcreative.com/tags/telegram1.html */
 
     /* Recent 2 */
-    document.getElementById("title11").innerHTML = 'CBS News is suspending its use of Twitter, per Forbes.'
-    document.getElementById("content11").innerHTML = 'The decision comes not long after Elon Musk took over the social networking site.'
-    document.getElementById("content011").innerHTML = 'Update: 11/20/22 3:02pm MST: CBS announces it is resuming Twitter operations.'
-    document.getElementById("link11").innerHTML = 'Read the article from Forbes'
-    document.getElementById("link11").href = 'https://www.forbes.com/sites/markjoyella/2022/11/19/cbs-news-suspends-all-activity-on-twitter-in-light-of-the-uncertainty-surrounding-platform-under-musk/?sh=38c8d66fff96'
-    document.getElementById("time11").innerHTML = 'Updated: 11/20/22 3:02pm MST | 11/19/22 1:21pm MST'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/56'
+    var l4 = "11"
+    document.getElementById("title"+l4).innerHTML = 'CBS News is suspending its use of Twitter, per Forbes.'
+    document.getElementById("content"+l4).innerHTML = 'The decision comes not long after Elon Musk took over the social networking site.'
+    document.getElementById("content0"+l4).innerHTML = 'Update: 11/20/22 3:02pm MST: CBS announces it is resuming Twitter operations.'
+    document.getElementById("link"+l4).innerHTML = 'Read the article from Forbes'
+    document.getElementById("link"+l4).href = 'https://www.forbes.com/sites/markjoyella/2022/11/19/cbs-news-suspends-all-activity-on-twitter-in-light-of-the-uncertainty-surrounding-platform-under-musk/?sh=38c8d66fff96'
+    document.getElementById("time"+l4).innerHTML = 'Updated: 11/20/22 3:02pm MST | 11/19/22 1:21pm MST'
+    document.getElementById("telegram"+l4).href = 'https://t.me/openteanews/56'
 
-    document.getElementById("rlink11").innerHTML = 'See the Tweet'
-    document.getElementById("rlink11").href = 'https://twitter.com/openteanews/status/1594405324294746113?s=20&t=mg9zg-TNWSe_N3-DA8DQnQ'
+    document.getElementById("rlink"+l4).innerHTML = 'See the Tweet'
+    document.getElementById("rlink"+l4).href = 'https://twitter.com/openteanews/status/1594405324294746113?s=20&t=mg9zg-TNWSe_N3-DA8DQnQ'
 
-    document.getElementById("rlink011").innerHTML = ''
-    document.getElementById("rlink011").href = ''
+    document.getElementById("rlink0"+l4).innerHTML = ''
+    document.getElementById("rlink0"+l4).href = ''
 
 
     /* Recent 3 */
-    document.getElementById("title111").innerHTML = 'Project Veritas Twitter account has been reinstated.'
-    document.getElementById("content111").innerHTML = ''
-    document.getElementById("content0111").innerHTML = ''
-    document.getElementById("link111").innerHTML = 'See their profile'
-    document.getElementById("link111").href = 'https://twitter.com/Project_Veritas'
-    document.getElementById("time111").innerHTML = '11/20/22 3:01pm MST'
-    document.getElementById("telegram111").href = 'https://t.me/openteanews/59'
+    var l3 = "111"
+    document.getElementById("title"+l3).innerHTML = 'Project Veritas Twitter account has been reinstated.'
+    document.getElementById("content"+l3).innerHTML = ''
+    document.getElementById("content0"+l3).innerHTML = ''
+    document.getElementById("link"+l3).innerHTML = 'See their profile'
+    document.getElementById("link"+l3).href = 'https://twitter.com/Project_Veritas'
+    document.getElementById("time"+l3).innerHTML = '11/20/22 3:01pm MST'
+    document.getElementById("telegram"+l3).href = 'https://t.me/openteanews/59'
 
-    document.getElementById("rlink111").innerHTML = ''
-    document.getElementById("rlink111").href = ''
+    document.getElementById("rlink"+l3).innerHTML = ''
+    document.getElementById("rlink"+l3).href = ''
 
-    document.getElementById("rlink0111").innerHTML = ''
-    document.getElementById("rlink0111").href = ''
+    document.getElementById("rlink0"+l3).innerHTML = ''
+    document.getElementById("rlink0"+l3).href = ''
 
 
 
 
      /* Recent 4 */
-     document.getElementById("title1111").innerHTML = 'California made a $192,000,000.00 deal for a "COVID19 Graphic Designer."'
-     document.getElementById("content1111").innerHTML = ''
-     document.getElementById("content01111").innerHTML = ''
-     document.getElementById("link1111").innerHTML = 'Read the Tweet'
-     document.getElementById("link1111").href = 'https://twitter.com/jborg_arts/status/1592983294974386176'
-     document.getElementById("time1111").innerHTML = '11/17/22 11:00am MST'
-     document.getElementById("telegram1111").href = 'https://t.me/openteanews/55'
+     var l2 = "1111"
+     document.getElementById("title"+l2).innerHTML = 'California made a $192,000,000.00 deal for a "COVID19 Graphic Designer."'
+     document.getElementById("content"+l2).innerHTML = ''
+     document.getElementById("content0"+l2).innerHTML = ''
+     document.getElementById("link"+l2).innerHTML = 'Read the Tweet'
+     document.getElementById("link"+l2).href = 'https://twitter.com/jborg_arts/status/1592983294974386176'
+     document.getElementById("time"+l2).innerHTML = '11/17/22 11:00am MST'
+     document.getElementById("telegram"+l2).href = 'https://t.me/openteanews/55'
  
-     document.getElementById("rlink1111").innerHTML = ''
-     document.getElementById("rlink1111").href = ''
+     document.getElementById("rlink"+l2).innerHTML = ''
+     document.getElementById("rlink"+l2).href = ''
  
-     document.getElementById("rlink01111").innerHTML = ''
-     document.getElementById("rlink01111").href = ''
+     document.getElementById("rlink0"+l2).innerHTML = ''
+     document.getElementById("rlink0"+l2).href = ''
  
      /* Recent 5 */
-
-     document.getElementById("title11111").innerHTML = 'Per the latest update from CA SOS (11/10/22), there are still 4,829,879 unprocessed ballots in California.'
-     document.getElementById("content11111").innerHTML = ''
-     document.getElementById("content011111").innerHTML = ''
-     document.getElementById("link11111").innerHTML = 'Read the Document'
-     document.getElementById("link11111").href = 'https://elections.cdn.sos.ca.gov/statewide-elections/2022-general/unprocessed-ballots-report.pdf'
-     document.getElementById("time11111").innerHTML = '11/13/22 2:53pm MST'
-     document.getElementById("telegram11111").href = 'https://t.me/openteanews/54'
+    var l1 = "11111"
+     document.getElementById("title"+l1).innerHTML = 'Per the latest update from CA SOS (11/10/22), there are still 4,829,879 unprocessed ballots in California.'
+     document.getElementById("content"+l1).innerHTML = ''
+     document.getElementById("content0"+l1).innerHTML = ''
+     document.getElementById("link"+l1).innerHTML = 'Read the Document'
+     document.getElementById("link"+l1).href = 'https://elections.cdn.sos.ca.gov/statewide-elections/2022-general/unprocessed-ballots-report.pdf'
+     document.getElementById("time"+l1).innerHTML = '11/13/22 2:53pm MST'
+     document.getElementById("telegram"+l1).href = 'https://t.me/openteanews/54'
  
-     document.getElementById("rlink11111").innerHTML = ''
-     document.getElementById("rlink11111").href = ''
+     document.getElementById("rlink"+l1).innerHTML = ''
+     document.getElementById("rlink"+l1).href = ''
  
-     document.getElementById("rlink011111").innerHTML = ''
-     document.getElementById("rlink011111").href = ''
+     document.getElementById("rlink0"+l1).innerHTML = ''
+     document.getElementById("rlink0"+l1).href = ''
  
  
  
