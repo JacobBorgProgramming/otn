51bc29ee0f13d488898b1338a2371f79b6aee49c
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-28 10:24:07 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,5 +1,22 @@
 function latest(){
-    var l4 = "1"
+
+    var l5 = "1"
+    document.getElementById("title"+l5).innerHTML = 'Red Cross opens shelter on the Big Island in response to the eruption of Mauna Loa.'
+    document.getElementById("content"+l5).innerHTML = ''
+    document.getElementById("content0"+l5).innerHTML = ''
+    document.getElementById("link"+l5).innerHTML = 'See the Tweet'
+    document.getElementById("link"+l5).href = 'https://twitter.com/HawaiiRedCross/status/1597267905099374592'
+    document.getElementById("time"+l5).innerHTML = '11/84/22 10:22am MST'
+    document.getElementById("telegram"+l5).href = 'https://t.me/openteanews/72'
+
+    document.getElementById("rlink"+l5).innerHTML = ''
+    document.getElementById("rlink"+l5).href = ''
+
+    document.getElementById("rlink0"+l5).innerHTML = ''
+    document.getElementById("rlink0"+l5).href = ''
+
+
+    var l4 = "11"
     document.getElementById("title"+l4).innerHTML = 'BlockFi files voluntary chapter 11.'
     document.getElementById("content"+l4).innerHTML = 'The crypto company announced its chapter 11 filing just 17 days after FTX, a crypto exchange, filed for chapter 11.'
     document.getElementById("content0"+l4).innerHTML = ''
@@ -14,7 +31,7 @@ function latest(){
     document.getElementById("rlink0"+l4).innerHTML = ''
     document.getElementById("rlink0"+l4).href = ''
 
-var l3 = "11"
+var l3 = "111"
     document.getElementById("title"+l3).innerHTML = 'Protests break out in Beijing, per Election Wizard.'
     document.getElementById("content"+l3).innerHTML = ''
     document.getElementById("content0"+l3).innerHTML = ''
@@ -31,7 +48,7 @@ var l3 = "11"
 
 
 
-    var l2 = "111"
+    var l2 = "1111"
     document.getElementById("title"+l2).innerHTML = 'Censorship Monitor:'
     document.getElementById("content"+l2).innerHTML = 'YouTube puts fact check from Wikipedia calling NWO a "conspiracy theory," on a video from the World Government Summit titled: "Are We Ready for a New World Order?".'
     document.getElementById("content0"+l2).innerHTML = ''
@@ -47,7 +64,7 @@ var l3 = "11"
     document.getElementById("rlink0"+l2).href = ''
 
     
-   var l1 = "1111"
+   var l1 = "11111"
     document.getElementById("title"+l1).innerHTML = '“FCC Bans Authorizations for Devices That Pose National Security Threat”'
     document.getElementById("content"+l1).innerHTML = 'Includes: Huawei & ZTE “(and that of their subsidiaries and affiliates)”'
     document.getElementById("content0"+l1).innerHTML = ''
@@ -64,41 +81,10 @@ var l3 = "11"
 
 
 
-    var l5 = "11111"
-    document.getElementById("title"+l5).innerHTML = 'Elon Musks "General Amnesty" Twitter poll has ended.'
-    document.getElementById("content"+l5).innerHTML = 'Yes: 72.4%'
-    document.getElementById("content0"+l5).innerHTML = 'No: 27.6%'
-    document.getElementById("link"+l5).innerHTML = 'See the Tweet'
-    document.getElementById("link"+l5).href = 'https://twitter.com/elonmusk/status/1595473875847942146?s=20&t=2G_YriM2tq9egnXwh6cY_w'
-    document.getElementById("time"+l5).innerHTML = '11/24/22 11:12am MST'
-    document.getElementById("telegram"+l5).href = 'https://t.me/openteanews/60'
-
-    document.getElementById("rlink"+l5).innerHTML = ''
-    document.getElementById("rlink"+l5).href = ''
-
-    document.getElementById("rlink0"+l5).innerHTML = ''
-    document.getElementById("rlink0"+l5).href = ''
 
     
     
     /*https://tea.borgcreative.com/tags/telegram1.html */
-
-
-    var l4 = "11111"
-    document.getElementById("title"+l4).innerHTML = 'CBS News is suspending its use of Twitter, per Forbes.'
-    document.getElementById("content"+l4).innerHTML = 'The decision comes not long after Elon Musk took over the social networking site.'
-    document.getElementById("content0"+l4).innerHTML = 'Update: 11/20/22 3:02pm MST: CBS announces it is resuming Twitter operations.'
-    document.getElementById("link"+l4).innerHTML = 'Read the article from Forbes'
-    document.getElementById("link"+l4).href = 'https://www.forbes.com/sites/markjoyella/2022/11/19/cbs-news-suspends-all-activity-on-twitter-in-light-of-the-uncertainty-surrounding-platform-under-musk/?sh=38c8d66fff96'
-    document.getElementById("time"+l4).innerHTML = 'Updated: 11/20/22 3:02pm MST | 11/19/22 1:21pm MST'
-    document.getElementById("telegram"+l4).href = 'https://t.me/openteanews/56'
-
-    document.getElementById("rlink"+l4).innerHTML = 'See the Tweet'
-    document.getElementById("rlink"+l4).href = 'https://twitter.com/openteanews/status/1594405324294746113?s=20&t=mg9zg-TNWSe_N3-DA8DQnQ'
-
-    document.getElementById("rlink0"+l4).innerHTML = ''
-    document.getElementById("rlink0"+l4).href = ''
-
 
 
 
