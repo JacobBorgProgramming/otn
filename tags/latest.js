b3646f385edd3a4568254c61d1a42c7a9f642785
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-28 10:16:14 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,5 +1,20 @@
 function latest(){
-var l3 = "1"
+    var l4 = "1"
+    document.getElementById("title"+l4).innerHTML = 'BlockFi files voluntary chapter 11.'
+    document.getElementById("content"+l4).innerHTML = 'The crypto exchange announced its chapter 11 filing just 17 days after FTX, another crypto exchange, filed for chapter 11.'
+    document.getElementById("content0"+l4).innerHTML = ''
+    document.getElementById("link"+l4).innerHTML = 'Read the release'
+    document.getElementById("link"+l4).href = 'https://blockfi.com/November28-ClientUpdate'
+    document.getElementById("time"+l4).innerHTML = '11/28/22 10:15am MST'
+    document.getElementById("telegram"+l4).href = 'https://t.me/openteanews/71'
+
+    document.getElementById("rlink"+l4).innerHTML = ''
+    document.getElementById("rlink"+l4).href = ''
+
+    document.getElementById("rlink0"+l4).innerHTML = ''
+    document.getElementById("rlink0"+l4).href = ''
+
+var l3 = "11"
     document.getElementById("title"+l3).innerHTML = 'Protests break out in Beijing, per Election Wizard.'
     document.getElementById("content"+l3).innerHTML = ''
     document.getElementById("content0"+l3).innerHTML = ''
@@ -16,7 +31,7 @@ var l3 = "1"
 
 
 
-    var l2 = "11"
+    var l2 = "111"
     document.getElementById("title"+l2).innerHTML = 'Censorship Monitor:'
     document.getElementById("content"+l2).innerHTML = 'YouTube puts fact check from Wikipedia calling NWO a "conspiracy theory," on a video from the World Government Summit titled: "Are We Ready for a New World Order?".'
     document.getElementById("content0"+l2).innerHTML = ''
@@ -32,7 +47,7 @@ var l3 = "1"
     document.getElementById("rlink0"+l2).href = ''
 
     
-   var l1 = "111"
+   var l1 = "1111"
     document.getElementById("title"+l1).innerHTML = '“FCC Bans Authorizations for Devices That Pose National Security Threat”'
     document.getElementById("content"+l1).innerHTML = 'Includes: Huawei & ZTE “(and that of their subsidiaries and affiliates)”'
     document.getElementById("content0"+l1).innerHTML = ''
@@ -49,7 +64,7 @@ var l3 = "1"
 
 
 
-    var l5 = "1111"
+    var l5 = "11111"
     document.getElementById("title"+l5).innerHTML = 'Elon Musks "General Amnesty" Twitter poll has ended.'
     document.getElementById("content"+l5).innerHTML = 'Yes: 72.4%'
     document.getElementById("content0"+l5).innerHTML = 'No: 27.6%'
