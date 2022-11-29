ac146d9055ec6caf2f40a4216bfb489d6c42770f
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-11-29 14:46:39 -0700

    Update censorship.js

--- tags/censorship.js
+++ tags/censorship.js
@@ -3,7 +3,7 @@ function censorship(){
 
     document.getElementById("cctitle1").innerHTML = 'Twitter suspended enforcing its C19 anti-misinformation policies Nov 23rd, 2022.'
     document.getElementById("cccontent1").innerHTML = 'Since Jan. 2020:'
-    document.getElementById("cccontent01").innerHTML = '11.72 accounts challenged; 11,230 accounts suspended; 97,674 content removed'
+    document.getElementById("cccontent01").innerHTML = '11.72m accounts challenged; 11,230 accounts suspended; 97,674 content removed'
     document.getElementById("cclink1").innerHTML = 'Read the Tweet'
     document.getElementById("cclink1").href = 'https://twitter.com/openteanews/status/1597490555134410752'
     document.getElementById("cctime1").innerHTML = '11/29/22 12:42pm MST'
