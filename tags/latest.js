335e01e10345da7bf02ed4eb58e494221252c2a6
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-12-02 23:38:22 -0700

    update

--- tags/latest.js
+++ tags/latest.js
@@ -1,6 +1,21 @@
 function latest(){
 
-    var l3 = "1"
+    var l4 = "1"
+    document.getElementById("title"+l4).innerHTML = 'Twitter Files part 1'
+    document.getElementById("content"+l4).innerHTML = 'Today Twitter released information about Twitter censorship and the Hunter Biden laptop story.'
+    document.getElementById("content0"+l4).innerHTML = ''
+    document.getElementById("link"+l4).innerHTML = 'Read the Tweet Thread'
+    document.getElementById("link"+l4).href = 'https://twitter.com/mtaibbi/status/1598822959866683394'
+    document.getElementById("time"+l4).innerHTML = '12/2/22 11:36pm MST'
+    document.getElementById("telegram"+l4).href = 'https://t.me/openteanews/71'
+
+    document.getElementById("rlink"+l4).innerHTML = 'Read our Archive'
+    document.getElementById("rlink"+l4).href = 'https://www.openteanews.com/archive/twitterfiles1.txt'
+
+    document.getElementById("rlink0"+l4).innerHTML = ''
+    document.getElementById("rlink0"+l4).href = ''
+
+    var l3 = "11"
     document.getElementById("title"+l3).innerHTML = 'The Parler/YE deal has been terminated.'
     document.getElementById("content"+l3).innerHTML = 'In October, YE (Formally Kanye West) agreed to purchase Parlement Technologies, the parent company of the Parler social platform.'
     document.getElementById("content0"+l3).innerHTML = ''
@@ -15,7 +30,7 @@ function latest(){
     document.getElementById("rlink0"+l3).innerHTML = ''
     document.getElementById("rlink0"+l3).href = ''
 
-    var l2 = "11"
+    var l2 = "111"
     document.getElementById("title"+l2).innerHTML = 'Elon Musk says that Twitter has interfered in elections.'
     document.getElementById("content"+l2).innerHTML = '"Exactly. The obvious reality, as long-time users know, is that Twitter has failed in trust & safety for a very long time and has interfered in elections. <p>Twitter 2.0 will be far more effective, transparent and even-handed."</p>'
     document.getElementById("content0"+l2).innerHTML = '-Elon Musk via Twitter'
@@ -30,7 +45,7 @@ function latest(){
     document.getElementById("rlink0"+l2).innerHTML = ''
     document.getElementById("rlink0"+l2).href = ''
 
-    var l1 = "111"
+    var l1 = "1111"
     document.getElementById("title"+l1).innerHTML = 'Former DC Corrections officer charged for use of unreasonable force.'
     document.getElementById("content"+l1).innerHTML = 'The incident resulted in a handcuffed inmate being injured by the former officer.'
     document.getElementById("content0"+l1).innerHTML = ''
@@ -46,7 +61,7 @@ function latest(){
     document.getElementById("rlink0"+l1).href = ''
 
 
-    var l5 = "1111"
+    var l5 = "11111"
     document.getElementById("title"+l5).innerHTML = 'Red Cross opens shelters on the Big Island in response to the eruption of Mauna Loa.'
     document.getElementById("content"+l5).innerHTML = ''
     document.getElementById("content0"+l5).innerHTML = ''
@@ -60,23 +75,6 @@ function latest(){
 
     document.getElementById("rlink0"+l5).innerHTML = ''
     document.getElementById("rlink0"+l5).href = ''
-
-
-    var l4 = "11111"
-    document.getElementById("title"+l4).innerHTML = 'BlockFi files voluntary chapter 11.'
-    document.getElementById("content"+l4).innerHTML = 'The crypto company announced its chapter 11 filing just 17 days after FTX, a crypto exchange, filed for chapter 11.'
-    document.getElementById("content0"+l4).innerHTML = ''
-    document.getElementById("link"+l4).innerHTML = 'Read the release'
-    document.getElementById("link"+l4).href = 'https://blockfi.com/November28-ClientUpdate'
-    document.getElementById("time"+l4).innerHTML = '11/28/22 10:15am MST'
-    document.getElementById("telegram"+l4).href = 'https://t.me/openteanews/71'
-
-    document.getElementById("rlink"+l4).innerHTML = ''
-    document.getElementById("rlink"+l4).href = ''
-
-    document.getElementById("rlink0"+l4).innerHTML = ''
-    document.getElementById("rlink0"+l4).href = ''
-
 
     /*https://tea.borgcreative.com/tags/telegram1.html */
 }
