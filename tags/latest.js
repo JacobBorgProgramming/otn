ba4880323a981cbd247a12d903cf5904b5536758
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-12-03 14:58:21 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,13 +1,28 @@
 function latest(){
+    var l5 = "1"
+    document.getElementById("title"+l5).innerHTML = 'Elon Musk considers the possibility that Twitter may have interfered in the 2022 election in Brazil.'
+    document.getElementById("content"+l5).innerHTML = ''
+    document.getElementById("content0"+l5).innerHTML = ''
+    document.getElementById("link"+l5).innerHTML = 'See the Tweet'
+    document.getElementById("link"+l5).href = 'https://twitter.com/elonmusk/status/1598990820665065472?s=20&t=KcI5YTYYT7M_NPY3td5qPg'
+    document.getElementById("time"+l5).innerHTML = '12/3/22 2:57pm MST'
+    document.getElementById("telegram"+l5).href = 'https://t.me/openteanews/91'
 
-    var l4 = "1"
+    document.getElementById("rlink"+l5).innerHTML = ''
+    document.getElementById("rlink"+l5).href = ''
+
+    document.getElementById("rlink0"+l5).innerHTML = ''
+    document.getElementById("rlink0"+l5).href = ''
+
+
+    var l4 = "11"
     document.getElementById("title"+l4).innerHTML = 'Twitter Files part 1'
     document.getElementById("content"+l4).innerHTML = 'Today Twitter released information about Twitter censorship and the Hunter Biden laptop story.'
     document.getElementById("content0"+l4).innerHTML = ''
     document.getElementById("link"+l4).innerHTML = 'Read the Tweet Thread'
     document.getElementById("link"+l4).href = 'https://twitter.com/mtaibbi/status/1598822959866683394'
     document.getElementById("time"+l4).innerHTML = '12/2/22 11:36pm MST'
-    document.getElementById("telegram"+l4).href = 'https://t.me/openteanews/88'
+    document.getElementById("telegram"+l4).href = 'https://t.me/openteanews/90'
 
     document.getElementById("rlink"+l4).innerHTML = 'Read our Archive'
     document.getElementById("rlink"+l4).href = 'https://www.openteanews.com/archive/twitterfiles1.txt'
@@ -15,7 +30,7 @@ function latest(){
     document.getElementById("rlink0"+l4).innerHTML = ''
     document.getElementById("rlink0"+l4).href = ''
 
-    var l3 = "11"
+    var l3 = "111"
     document.getElementById("title"+l3).innerHTML = 'The Parler/YE deal has been terminated.'
     document.getElementById("content"+l3).innerHTML = 'In October, YE (Formally Kanye West) agreed to purchase Parlement Technologies, the parent company of the Parler social platform.'
     document.getElementById("content0"+l3).innerHTML = ''
@@ -30,7 +45,7 @@ function latest(){
     document.getElementById("rlink0"+l3).innerHTML = ''
     document.getElementById("rlink0"+l3).href = ''
 
-    var l2 = "111"
+    var l2 = "1111"
     document.getElementById("title"+l2).innerHTML = 'Elon Musk says that Twitter has interfered in elections.'
     document.getElementById("content"+l2).innerHTML = '"Exactly. The obvious reality, as long-time users know, is that Twitter has failed in trust & safety for a very long time and has interfered in elections. <p>Twitter 2.0 will be far more effective, transparent and even-handed."</p>'
     document.getElementById("content0"+l2).innerHTML = '-Elon Musk via Twitter'
@@ -45,7 +60,7 @@ function latest(){
     document.getElementById("rlink0"+l2).innerHTML = ''
     document.getElementById("rlink0"+l2).href = ''
 
-    var l1 = "1111"
+    var l1 = "11111"
     document.getElementById("title"+l1).innerHTML = 'Former DC Corrections officer charged for use of unreasonable force.'
     document.getElementById("content"+l1).innerHTML = 'The incident resulted in a handcuffed inmate being injured by the former officer.'
     document.getElementById("content0"+l1).innerHTML = ''
@@ -59,22 +74,6 @@ function latest(){
 
     document.getElementById("rlink0"+l1).innerHTML = ''
     document.getElementById("rlink0"+l1).href = ''
-
-
-    var l5 = "11111"
-    document.getElementById("title"+l5).innerHTML = 'Red Cross opens shelters on the Big Island in response to the eruption of Mauna Loa.'
-    document.getElementById("content"+l5).innerHTML = ''
-    document.getElementById("content0"+l5).innerHTML = ''
-    document.getElementById("link"+l5).innerHTML = 'See the Tweet'
-    document.getElementById("link"+l5).href = 'https://twitter.com/HawaiiRedCross/status/1597267905099374592'
-    document.getElementById("time"+l5).innerHTML = '11/28/22 10:22am MST'
-    document.getElementById("telegram"+l5).href = 'https://t.me/openteanews/72'
-
-    document.getElementById("rlink"+l5).innerHTML = ''
-    document.getElementById("rlink"+l5).href = ''
-
-    document.getElementById("rlink0"+l5).innerHTML = ''
-    document.getElementById("rlink0"+l5).href = ''
 
     /*https://tea.borgcreative.com/tags/telegram1.html */
 }
