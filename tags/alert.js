feedcfc1f004eecda134eeb720041d6e495175e9
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-12-03 14:49:40 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,7 +1,7 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = '1036 views: our most successful week yet! Thank you for supporting independent media. '
-    document.getElementById("alink").innerHTML = 'View the data'
-    document.getElementById("alink").href = 'https://www.openteanews.com/reach.html'
+    document.getElementById("acontent1").innerHTML = 'Twitter Files confirm politically targeted censorship: "...in 2020, requests from both the Trump White House and the Biden campaign were received and honored..." '
+    document.getElementById("alink").innerHTML = 'Read the Tweet thread (EXPLICIT LANGUAGE)'
+    document.getElementById("alink").href = 'https://www.openteanews.com/archive/twitterfiles1.txt'
 }
 
 /*
