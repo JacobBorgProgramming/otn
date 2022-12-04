010dea2bd27590335612b60cab6511a8e6adf794
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-12-03 19:19:50 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,5 +1,5 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = 'Elon Musk posted a Twitter poll on whether Assange and Snowden should be pardoned.'
+    document.getElementById("acontent1").innerHTML = 'Elon Musk posted a Twitter poll on whether Assange and Snowden should be pardoned. '
     document.getElementById("alink").innerHTML = 'See the Tweet'
     document.getElementById("alink").href = 'https://twitter.com/elonmusk/status/1599224347121500160?s=20&t=l9TUiM39EtI-r24Rt-Foeg'
 }
