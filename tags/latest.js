7d1d4a1acd6da159c99d2b312dac7cad9aff2a61
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-12-03 19:21:41 -0700

    Update latest.js
    Made editing faster

--- tags/latest.js
+++ tags/latest.js
@@ -1,9 +1,14 @@
 function latest(){
+    var tweet = "Read the Tweet"
+    var doc = "Read the document"
+    var release = "Read the release"
+    var video = "Watch the video"
+
     var l5 = "1"
     document.getElementById("title"+l5).innerHTML = 'Elon Musk considers the possibility that Twitter may have interfered in the 2022 election in Brazil.'
     document.getElementById("content"+l5).innerHTML = ''
     document.getElementById("content0"+l5).innerHTML = ''
-    document.getElementById("link"+l5).innerHTML = 'See the Tweet'
+    document.getElementById("link"+l5).innerHTML = tweet
     document.getElementById("link"+l5).href = 'https://twitter.com/elonmusk/status/1598990820665065472?s=20&t=KcI5YTYYT7M_NPY3td5qPg'
     document.getElementById("time"+l5).innerHTML = '12/3/22 2:57pm MST'
     document.getElementById("telegram"+l5).href = 'https://t.me/openteanews/91'
@@ -19,7 +24,7 @@ function latest(){
     document.getElementById("title"+l4).innerHTML = 'Twitter Files part 1'
     document.getElementById("content"+l4).innerHTML = 'Today Twitter released information about Twitter censorship and the Hunter Biden laptop story.'
     document.getElementById("content0"+l4).innerHTML = ''
-    document.getElementById("link"+l4).innerHTML = 'Read the Tweet Thread'
+    document.getElementById("link"+l4).innerHTML = tweet + ' Thread'
     document.getElementById("link"+l4).href = 'https://twitter.com/mtaibbi/status/1598822959866683394'
     document.getElementById("time"+l4).innerHTML = '12/2/22 11:36pm MST'
     document.getElementById("telegram"+l4).href = 'https://t.me/openteanews/90'
@@ -34,7 +39,7 @@ function latest(){
     document.getElementById("title"+l3).innerHTML = 'The Parler/YE deal has been terminated.'
     document.getElementById("content"+l3).innerHTML = 'In October, YE (Formally Kanye West) agreed to purchase Parlement Technologies, the parent company of the Parler social platform.'
     document.getElementById("content0"+l3).innerHTML = ''
-    document.getElementById("link"+l3).innerHTML = 'See the Tweet'
+    document.getElementById("link"+l3).innerHTML = tweet
     document.getElementById("link"+l3).href = 'https://twitter.com/parler_app/status/1598422341553364993'
     document.getElementById("time"+l3).innerHTML = '12/1/22 5:30pm MST'
     document.getElementById("telegram"+l3).href = 'https://t.me/openteanews/81'
@@ -49,7 +54,7 @@ function latest(){
     document.getElementById("title"+l2).innerHTML = 'Elon Musk says that Twitter has interfered in elections.'
     document.getElementById("content"+l2).innerHTML = '"Exactly. The obvious reality, as long-time users know, is that Twitter has failed in trust & safety for a very long time and has interfered in elections. <p>Twitter 2.0 will be far more effective, transparent and even-handed."</p>'
     document.getElementById("content0"+l2).innerHTML = '-Elon Musk via Twitter'
-    document.getElementById("link"+l2).innerHTML = 'Read the Tweet'
+    document.getElementById("link"+l2).innerHTML = tweet
     document.getElementById("link"+l2).href = 'https://twitter.com/elonmusk/status/1598004480066621441?s=20&t=lEtETTL0_6c3-2n_reJB2w'
     document.getElementById("time"+l2).innerHTML = '11/30/22 10:39am MST'
     document.getElementById("telegram"+l2).href = 'https://t.me/openteanews/79'
@@ -64,7 +69,7 @@ function latest(){
     document.getElementById("title"+l1).innerHTML = 'Former DC Corrections officer charged for use of unreasonable force.'
     document.getElementById("content"+l1).innerHTML = 'The incident resulted in a handcuffed inmate being injured by the former officer.'
     document.getElementById("content0"+l1).innerHTML = ''
-    document.getElementById("link"+l1).innerHTML = 'Read the release'
+    document.getElementById("link"+l1).innerHTML = release
     document.getElementById("link"+l1).href = 'https://www.justice.gov/opa/pr/former-dc-corrections-officer-charged-federal-civil-rights-violation-assaulting-handcuffed'
     document.getElementById("time"+l1).innerHTML = '11/30/22 10:29am MST'
     document.getElementById("telegram"+l1).href = 'https://t.me/openteanews/78'
