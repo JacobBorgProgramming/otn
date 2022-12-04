ac70bae15583e279faa1130efa09dd4e974be3bc
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-12-03 19:17:44 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,7 +1,7 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = '"Illegitimate President" trends on Twitter.'
-    document.getElementById("alink").innerHTML = ''
-    document.getElementById("alink").href = ''
+    document.getElementById("acontent1").innerHTML = 'Elon Musk posted a Twitter poll on whether Assange and Snowden should be pardoned.'
+    document.getElementById("alink").innerHTML = 'See the Tweet'
+    document.getElementById("alink").href = 'https://twitter.com/elonmusk/status/1599224347121500160?s=20&t=l9TUiM39EtI-r24Rt-Foeg'
 }
 
 /*
