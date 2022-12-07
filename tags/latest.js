471acfd9d2e8c9988cddb2502504e493f8f564d5
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-12-07 12:50:29 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -5,8 +5,24 @@ function latest(){
     var video = 'Watch the video <svg class="icon icon-tabler icon-tabler-video" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M15 10l4.553 -2.276a1 1 0 0 1 1.447 .894v6.764a1 1 0 0 1 -1.447 .894l-4.553 -2.276v-4z"></path><rect x="3" y="6" width="12" height="12" rx="2"></rect></svg>'
     var article = 'Read the article <svg class="icon icon-tabler icon-tabler-external-link" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5"></path><line x1="10" y1="14" x2="20" y2="4"></line><polyline points="15 4 20 4 20 9"></polyline></svg>'
     var data = 'See the data <svg class="icon icon-tabler icon-tabler-chart-line" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="4" y1="19" x2="20" y2="19"></line><polyline points="4 15 8 9 12 11 16 6 20 10"></polyline></svg>'
+    
+    
+    var l3 = "1"
+    document.getElementById("title"+l3).innerHTML = 'House Ethics Committee opens investigation into Rep. AOC.'
+    document.getElementById("content"+l3).innerHTML = ''
+    document.getElementById("content0"+l3).innerHTML = ''
+    document.getElementById("link"+l3).innerHTML = release
+    document.getElementById("link"+l3).href = 'https://ethics.house.gov/press-releases/statement-acting-chairwoman-and-acting-ranking-member-committee-ethics-regarding-3'
+    document.getElementById("time"+l3).innerHTML = '12/7/22 12:50pm MST'
+    document.getElementById("telegram"+l3).href = 'https://t.me/openteanews/102'
 
-    var l2 = "1"
+    document.getElementById("rlink"+l3).innerHTML = ''
+     document.getElementById("rlink"+l3).href = ''
+
+    document.getElementById("rlink0"+l3).innerHTML = ''
+    document.getElementById("rlink0"+l3).href = ''
+
+    var l2 = "11"
     document.getElementById("title"+l2).innerHTML = 'Kevin McCarthy (R) has released a plan for oversight and accountability.'
     document.getElementById("content"+l2).innerHTML = ''
     document.getElementById("content0"+l2).innerHTML = ''
@@ -21,7 +37,7 @@ function latest(){
     document.getElementById("rlink0"+l2).innerHTML = ''
     document.getElementById("rlink0"+l2).href = ''
 
-    var l1 = "11"
+    var l1 = "111"
     document.getElementById("title"+l1).innerHTML = 'Time Magazine names Volodymyr Zelensky, the president of Ukraine, "Person of the Year."'
     document.getElementById("content"+l1).innerHTML = ''
     document.getElementById("content0"+l1).innerHTML = ''
@@ -37,7 +53,7 @@ function latest(){
     document.getElementById("rlink0"+l1).href = ''
 
     
-    var l5 = "111"
+    var l5 = "1111"
     document.getElementById("title"+l5).innerHTML = 'Elon Musk considers the possibility that Twitter may have interfered in the 2022 election in Brazil.'
     document.getElementById("content"+l5).innerHTML = ''
     document.getElementById("content0"+l5).innerHTML = ''
@@ -53,7 +69,7 @@ function latest(){
     document.getElementById("rlink0"+l5).href = ''
 
 
-    var l4 = "1111"
+    var l4 = "11111"
     document.getElementById("title"+l4).innerHTML = 'Twitter Files part 1'
     document.getElementById("content"+l4).innerHTML = 'Today Twitter released information about Twitter censorship and the Hunter Biden laptop story.'
     document.getElementById("content0"+l4).innerHTML = ''
@@ -67,21 +83,6 @@ function latest(){
 
     document.getElementById("rlink0"+l4).innerHTML = ''
     document.getElementById("rlink0"+l4).href = ''
-
-    var l3 = "11111"
-    document.getElementById("title"+l3).innerHTML = 'The Parler/YE deal has been terminated.'
-    document.getElementById("content"+l3).innerHTML = 'In October, YE (Formally Kanye West) agreed to purchase Parlement Technologies, the parent company of the Parler social platform.'
-    document.getElementById("content0"+l3).innerHTML = ''
-    document.getElementById("link"+l3).innerHTML = tweet
-    document.getElementById("link"+l3).href = 'https://twitter.com/parler_app/status/1598422341553364993'
-    document.getElementById("time"+l3).innerHTML = '12/1/22 5:30pm MST'
-    document.getElementById("telegram"+l3).href = 'https://t.me/openteanews/81'
-
-    document.getElementById("rlink"+l3).innerHTML = ''
-     document.getElementById("rlink"+l3).href = ''
-
-    document.getElementById("rlink0"+l3).innerHTML = ''
-    document.getElementById("rlink0"+l3).href = ''
 
     /*https://tea.borgcreative.com/tags/telegram1.html */
 }
