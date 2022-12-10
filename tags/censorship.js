3229f5c50478392edf1969c7b9ee038fe98b0e92
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-12-10 11:51:04 -0700

    update

--- tags/censorship.js
+++ tags/censorship.js
@@ -8,12 +8,12 @@ function censorship(){
 
     /* censorship 1 */
 
-    document.getElementById("cctitle1").innerHTML = 'Twitter suspended enforcing its C19 anti-misinformation policies Nov 23rd, 2022.'
-    document.getElementById("cccontent1").innerHTML = 'Since Jan. 2020:'
-    document.getElementById("cccontent01").innerHTML = '11.72m accounts challenged; 11,230 accounts suspended; 97,674 content removed'
-    document.getElementById("cclink1").innerHTML = tweet
-    document.getElementById("cclink1").href = 'https://twitter.com/openteanews/status/1597490555134410752'
-    document.getElementById("cctime1").innerHTML = '11/29/22 12:42pm MST'
+    document.getElementById("cctitle1").innerHTML = 'The Twitter Files compiled'
+    document.getElementById("cccontent1").innerHTML = 'On November 28th, 2022, Twitter owner Elon Musk tweeted:'
+    document.getElementById("cccontent01").innerHTML = '"The Twitter Files on free speech suppression soon to be published on Twitter itself. The public deserves to know what really happened …"'
+    document.getElementById("cclink1").innerHTML = doc
+    document.getElementById("cclink1").href = 'https://www.openteanews.com/archive/2022/twitterfiles'
+    document.getElementById("cctime1").innerHTML = '12/10/22 11:49am MST'
 
     /* censorship 2 */
 
