b4dccc7a174a963d19aa4894b2c36a34cd6d5f17
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2023-02-01 17:43:34 -0700

    updates

--- tags/alert.js
+++ tags/alert.js
@@ -7,7 +7,7 @@ function alert(){
     var data = 'See the data <svg class="icon icon-tabler icon-tabler-chart-line" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="4" y1="19" x2="20" y2="19"></line><polyline points="4 15 8 9 12 11 16 6 20 10"></polyline></svg>'
 
 
-    document.getElementById("acontent1").innerHTML = 'Have a happy 2023! '
+    document.getElementById("acontent1").innerHTML = ''
     document.getElementById("alink").innerHTML = ''
     document.getElementById("alink").href = ''
 }
