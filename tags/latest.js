3774ea49a9a61aed623ac1e31f0b00bc175d1334
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2023-03-01 11:01:27 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -6,8 +6,23 @@ function latest(){
     var article = 'Read the article <svg class="icon icon-tabler icon-tabler-external-link" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5"></path><line x1="10" y1="14" x2="20" y2="4"></line><polyline points="15 4 20 4 20 9"></polyline></svg>'
     var data = 'See the data <svg class="icon icon-tabler icon-tabler-chart-line" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="4" y1="19" x2="20" y2="19"></line><polyline points="4 15 8 9 12 11 16 6 20 10"></polyline></svg>'
     
+    var l5 = "1"
+    document.getElementById("title"+l5).innerHTML = 'U.S. Officials Urge China Not to Ship Arms to Russia '
+    document.getElementById("content"+l5).innerHTML = ''
+    document.getElementById("content0"+l5).innerHTML = ''
+    document.getElementById("link"+l5).innerHTML = release
+    document.getElementById("link"+l5).href = 'https://www.defense.gov/News/News-Stories/Article/Article/3314089/us-officials-urge-china-not-to-ship-arms-to-russia/'
+    document.getElementById("time"+l5).innerHTML = '3/1/23 11:00am MST'
+    document.getElementById("telegram"+l5).href = 'https://t.me/openteanews/173'
 
-    var l4 = "1"
+    document.getElementById("rlink"+l5).innerHTML = 'DOD Update: "General Says Chinese Surveillance Balloon Now Over Center of U.S."'
+    document.getElementById("rlink"+l5).href = 'https://www.defense.gov/News/News-Stories/Article/Article/3288103/general-says-chinese-surveillance-balloon-now-over-center-of-us/'
+
+    document.getElementById("rlink0"+l5).innerHTML = ''
+    document.getElementById("rlink0"+l5).href = ''
+
+
+    var l4 = "11"
     document.getElementById("title"+l4).innerHTML = 'Ohio EPA releases latest water test results from East Palestine.'
     document.getElementById("content"+l4).innerHTML = ''
     document.getElementById("content0"+l4).innerHTML = ''
@@ -22,7 +37,7 @@ function latest(){
     document.getElementById("rlink0"+l4).innerHTML = ''
     document.getElementById("rlink0"+l4).href = '' 
     
-        var l3 = "11"
+        var l3 = "111"
     document.getElementById("title"+l3).innerHTML = 'FEMA advises Governor Mike DeWine that Ohio is not eligable for FEMA assistance at this time.'
     document.getElementById("content"+l3).innerHTML = 'Ohio is still dealing with the after effects of the February 3 freight train derailment in East Palestine, Ohio.'
     document.getElementById("content0"+l3).innerHTML = '"The chemical plume of butyl acrylate in the Ohio River is currently located near Gallipolis, Ohio, and will be near Huntington, West Virginia, sometime tomorrow."<p>-Ohio EMA</p>'
@@ -38,7 +53,7 @@ function latest(){
     document.getElementById("rlink0"+l3).href = ''
 
 
-    var l2 = "111"
+    var l2 = "1111"
     document.getElementById("title"+l2).innerHTML = 'DOD confirms that U.S. fighter aircraft shot down the high altitude surveillance balloon.'
     document.getElementById("content"+l2).innerHTML = '<p>POTUS gave his orders on Wednesday to take action the moment it could be down without endangering any lives</p><p>The balloon was taken down off the coast of South Carolina in US airspace</p> '
     document.getElementById("content0"+l2).innerHTML = ''
@@ -53,7 +68,7 @@ function latest(){
     document.getElementById("rlink0"+l2).innerHTML = ''
     document.getElementById("rlink0"+l2).href = ''
 
-    var l1 = "1111"
+    var l1 = "11111"
     document.getElementById("title"+l1).innerHTML = 'Per Disclose.tv: The Chinese spy balloon has been shot down. '
     document.getElementById("content"+l1).innerHTML = ''
     document.getElementById("content0"+l1).innerHTML = ''
@@ -69,21 +84,6 @@ function latest(){
     document.getElementById("rlink0"+l1).href = ''
 
 
-
-    var l5 = "11111"
-    document.getElementById("title"+l5).innerHTML = 'Chinese High-Altitude Surveillance Balloon in US airspace. '
-    document.getElementById("content"+l5).innerHTML = 'US officials have decided not to use "kinetic force" against the balloon due to concern for civilian safety.'
-    document.getElementById("content0"+l5).innerHTML = ''
-    document.getElementById("link"+l5).innerHTML = release
-    document.getElementById("link"+l5).href = 'https://www.defense.gov/News/News-Stories/Article/Article/3287177/us-tracking-high-altitude-surveillance-balloon/'
-    document.getElementById("time"+l5).innerHTML = '2/2/23 6:44pm MST | Updated 2/3/23 5:54pm'
-    document.getElementById("telegram"+l5).href = 'https://t.me/openteanews/156'
-
-    document.getElementById("rlink"+l5).innerHTML = 'DOD Update: "General Says Chinese Surveillance Balloon Now Over Center of U.S."'
-    document.getElementById("rlink"+l5).href = 'https://www.defense.gov/News/News-Stories/Article/Article/3288103/general-says-chinese-surveillance-balloon-now-over-center-of-us/'
-
-    document.getElementById("rlink0"+l5).innerHTML = ''
-    document.getElementById("rlink0"+l5).href = ''
 
 
 
