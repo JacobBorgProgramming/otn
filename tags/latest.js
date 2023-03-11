ebace20b76e3d13d6272331bbf6f1cf75844381a
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2023-03-10 17:18:14 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -6,7 +6,22 @@ function latest(){
     var article = 'Read the article <svg class="icon icon-tabler icon-tabler-external-link" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5"></path><line x1="10" y1="14" x2="20" y2="4"></line><polyline points="15 4 20 4 20 9"></polyline></svg>'
     var data = 'See the data <svg class="icon icon-tabler icon-tabler-chart-line" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="4" y1="19" x2="20" y2="19"></line><polyline points="4 15 8 9 12 11 16 6 20 10"></polyline></svg>'
     
-    var l1 = "1"
+    var l2 = "1"
+    document.getElementById("title"+l2).innerHTML = 'Silicon Valley Bank (SVB) has collapsed; FDIC has taken control.'
+    document.getElementById("content"+l2).innerHTML = 'SVB had ~$209 billion in total assets as of December 31st, 2022.'
+    document.getElementById("content0"+l2).innerHTML = ''
+    document.getElementById("link"+l2).innerHTML = ''
+    document.getElementById("link"+l2).href = ''
+    document.getElementById("time"+l2).innerHTML = '3/10/22 5:18pm MST'
+    document.getElementById("telegram"+l2).href = 'https://t.me/openteanews/175'
+
+    document.getElementById("rlink"+l2).innerHTML = ''
+    document.getElementById("rlink"+l2).href = ''
+
+    document.getElementById("rlink0"+l2).innerHTML = ''
+    document.getElementById("rlink0"+l2).href = ''
+
+    var l1 = "11"
     document.getElementById("title"+l1).innerHTML = 'Speaker McCarthy via Twitter: '
     document.getElementById("content"+l1).innerHTML = '<p>"🚨 The House just approved an audit of Biden’s wasteful spending, which is driving up the cost of nearly everything you buy.</p><p>Every time he writes an executive order, you deserve to know exactly how much it’s going to cost you as a taxpayer.</p>'
     document.getElementById("content0"+l1).innerHTML = ''
@@ -21,7 +36,7 @@ function latest(){
     document.getElementById("rlink0"+l1).innerHTML = ''
     document.getElementById("rlink0"+l1).href = ''
 
-    var l5 = "11"
+    var l5 = "111"
     document.getElementById("title"+l5).innerHTML = 'U.S. Officials Urge China Not to Ship Arms to Russia '
     document.getElementById("content"+l5).innerHTML = ''
     document.getElementById("content0"+l5).innerHTML = ''
@@ -37,7 +52,7 @@ function latest(){
     document.getElementById("rlink0"+l5).href = ''
 
 
-    var l4 = "111"
+    var l4 = "1111"
     document.getElementById("title"+l4).innerHTML = 'Ohio EPA releases latest water test results from East Palestine.'
     document.getElementById("content"+l4).innerHTML = ''
     document.getElementById("content0"+l4).innerHTML = ''
@@ -52,7 +67,7 @@ function latest(){
     document.getElementById("rlink0"+l4).innerHTML = ''
     document.getElementById("rlink0"+l4).href = '' 
     
-        var l3 = "1111"
+        var l3 = "11111"
     document.getElementById("title"+l3).innerHTML = 'FEMA advises Governor Mike DeWine that Ohio is not eligable for FEMA assistance at this time.'
     document.getElementById("content"+l3).innerHTML = 'Ohio is still dealing with the after effects of the February 3 freight train derailment in East Palestine, Ohio.'
     document.getElementById("content0"+l3).innerHTML = '"The chemical plume of butyl acrylate in the Ohio River is currently located near Gallipolis, Ohio, and will be near Huntington, West Virginia, sometime tomorrow."<p>-Ohio EMA</p>'
@@ -68,20 +83,6 @@ function latest(){
     document.getElementById("rlink0"+l3).href = ''
 
 
-    var l2 = "11111"
-    document.getElementById("title"+l2).innerHTML = 'DOD confirms that U.S. fighter aircraft shot down the high altitude surveillance balloon.'
-    document.getElementById("content"+l2).innerHTML = '<p>POTUS gave his orders on Wednesday to take action the moment it could be down without endangering any lives</p><p>The balloon was taken down off the coast of South Carolina in US airspace</p> '
-    document.getElementById("content0"+l2).innerHTML = ''
-    document.getElementById("link"+l2).innerHTML = release
-    document.getElementById("link"+l2).href = 'https://www.defense.gov/News/Releases/Release/Article/3288535/statement-from-secretary-of-defense-lloyd-j-austin-iii/'
-    document.getElementById("time"+l2).innerHTML = '2/4/22 1:55pm MST'
-    document.getElementById("telegram"+l2).href = 'https://t.me/openteanews/160'
-
-    document.getElementById("rlink"+l2).innerHTML = ''
-    document.getElementById("rlink"+l2).href = ''
-
-    document.getElementById("rlink0"+l2).innerHTML = ''
-    document.getElementById("rlink0"+l2).href = ''
 
 
 
