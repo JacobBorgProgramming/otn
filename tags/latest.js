bd188bf2b1bee8ba5e9c1b75d1c714f9a6af04ad
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2023-03-31 11:25:56 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -6,7 +6,24 @@ function latest(){
     var article = 'Read the article <svg class="icon icon-tabler icon-tabler-external-link" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5"></path><line x1="10" y1="14" x2="20" y2="4"></line><polyline points="15 4 20 4 20 9"></polyline></svg>'
     var data = 'See the data <svg class="icon icon-tabler icon-tabler-chart-line" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="4" y1="19" x2="20" y2="19"></line><polyline points="4 15 8 9 12 11 16 6 20 10"></polyline></svg>'
     
-    var l4 = "1"
+    var l5 = "1"
+    document.getElementById("title"+l5).innerHTML = '"Justice Department and EPA File Complaint Against Norfolk Southern for Unlawful Discharge of Pollutants and Hazardous Substances in East Palestine Derailment"'
+    document.getElementById("content"+l5).innerHTML = ''
+    document.getElementById("content0"+l5).innerHTML = ''
+    document.getElementById("link"+l5).innerHTML = release
+    document.getElementById("link"+l5).href = 'https://www.justice.gov/opa/pr/justice-department-and-epa-file-complaint-against-norfolk-southern-unlawful-discharge'
+    document.getElementById("time"+l5).innerHTML = '3/31/23 11:25am MST'
+    document.getElementById("telegram"+l5).href = 'https://t.me/openteanews/'
+
+    document.getElementById("rlink"+l5).innerHTML = ''
+    document.getElementById("rlink"+l5).href = ''
+
+    document.getElementById("rlink0"+l5).innerHTML = ''
+    document.getElementById("rlink0"+l5).href = ''
+
+
+
+    var l4 = "11"
     document.getElementById("title"+l4).innerHTML = 'AI Bias?'
     document.getElementById("content"+l4).innerHTML = 'In recent weeks, AI chat and prompt tools have become quite a popular discussion topic. These tools developers have said that the tools can be used to increase productivity and may even replace journalists. Due to this, we have taken a look into the possible biases that AI could have.'
     document.getElementById("content0"+l4).innerHTML = 'We have conducted research into AI bias, and we encourage you to review our interactions with the Bing AI.'
@@ -21,7 +38,7 @@ function latest(){
     document.getElementById("rlink0"+l4).innerHTML = ''
     document.getElementById("rlink0"+l4).href = '' 
     
-    var l3 = "11"
+    var l3 = "111"
     document.getElementById("title"+l3).innerHTML = 'Twitters press contact email really does auto reply “💩”'
     document.getElementById("content"+l3).innerHTML = ''
     document.getElementById("link"+l3).innerHTML = tweet
@@ -36,7 +53,7 @@ function latest(){
     document.getElementById("rlink0"+l3).href = ''
 
 
-    var l2 = "111"
+    var l2 = "1111"
     document.getElementById("title"+l2).innerHTML = 'Silicon Valley Bank (SVB) has collapsed; FDIC has taken control.'
     document.getElementById("content"+l2).innerHTML = 'SVB had ~$209 billion in total assets as of December 31st, 2022.'
     document.getElementById("content0"+l2).innerHTML = ''
@@ -51,7 +68,7 @@ function latest(){
     document.getElementById("rlink0"+l2).innerHTML = ''
     document.getElementById("rlink0"+l2).href = ''
 
-    var l1 = "1111"
+    var l1 = "11111"
     document.getElementById("title"+l1).innerHTML = 'Speaker McCarthy via Twitter: '
     document.getElementById("content"+l1).innerHTML = '<p>"🚨 The House just approved an audit of Biden’s wasteful spending, which is driving up the cost of nearly everything you buy.</p><p>Every time he writes an executive order, you deserve to know exactly how much it’s going to cost you as a taxpayer.</p>'
     document.getElementById("content0"+l1).innerHTML = ''
@@ -65,21 +82,6 @@ function latest(){
 
     document.getElementById("rlink0"+l1).innerHTML = ''
     document.getElementById("rlink0"+l1).href = ''
-
-    var l5 = "11111"
-    document.getElementById("title"+l5).innerHTML = 'U.S. Officials Urge China Not to Ship Arms to Russia '
-    document.getElementById("content"+l5).innerHTML = ''
-    document.getElementById("content0"+l5).innerHTML = ''
-    document.getElementById("link"+l5).innerHTML = release
-    document.getElementById("link"+l5).href = 'https://www.defense.gov/News/News-Stories/Article/Article/3314089/us-officials-urge-china-not-to-ship-arms-to-russia/'
-    document.getElementById("time"+l5).innerHTML = '3/1/23 11:00am MST'
-    document.getElementById("telegram"+l5).href = 'https://t.me/openteanews/173'
-
-    document.getElementById("rlink"+l5).innerHTML = ''
-    document.getElementById("rlink"+l5).href = ''
-
-    document.getElementById("rlink0"+l5).innerHTML = ''
-    document.getElementById("rlink0"+l5).href = ''
 
 
    
