8ba8c2587353c49818dc04ea4a79859e4702eb80
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2024-01-19 14:27:28 -0700

    Began working on fixes

--- tags/funfacts.js
+++ tags/funfacts.js
@@ -1,8 +1,8 @@
 function funfacts(){
     /* older 1 */
 
-    document.getElementById("fftitle1").innerHTML = 'Article I, section 8, clause 17 of the Constitution'
-    document.getElementById("ffcontent1").innerHTML = '<p>"To exercise exclusive Legislation in all Cases whatsoever, over such District (not exceeding ten Miles square) as may, by Cession of particular States, and the Acceptance of Congress, become the Seat of Government of the United States, and to exercise like Authority over all Places purchased by the Consent of the Legislature of the State in which the Same shall be, for the Erection of Forts, Magazines, Arsenals, dock-Yards, and other needful Buildings;"</p>'
+    document.getElementById("fftitle1").innerHTML = 'Amendment I of the US Constitution'
+    document.getElementById("ffcontent1").innerHTML = '<p>"Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the government for a redress of grievances."</p>'
     document.getElementById("ffcontent2").innerHTML = '<p></p>'
     document.getElementById("fflink1").innerHTML = ''
     document.getElementById("fflink1").href = ''
@@ -10,7 +10,7 @@ function funfacts(){
 
     /* older 2 */
 
-    document.getElementById("fftitle2").innerHTML = 'Open Tea News, originally Meme Tea News, is now one year old!'
+    document.getElementById("fftitle2").innerHTML = 'Open Tea News, originally Meme Tea News, is now two years old! How time flies!'
     document.getElementById("ffcontent3").innerHTML = '<p>The <a href="https://tea.borgcreative.com/" target="_blank">original website</a>, launched December 12th, 2021.</p>'
     document.getElementById("ffcontent4").innerHTML = '<p>Since the original launch, we have made many technical improvements and added new features!</p>'
     document.getElementById("fflink2").innerHTML = ''
@@ -18,6 +18,6 @@ function funfacts(){
     document.getElementById("fftime2").innerHTML = ''
 
 
-    
+
 
 }
--- tags/meme.js
+++ tags/meme.js
@@ -1,14 +1,14 @@
 function memes(){
     /* Top most entry */
     document.getElementById("mtitle1").innerHTML = ''
-    document.getElementById("mcontent1").src = 'https://www.openteanews.com/assets/img/manoquality.png'
+    document.getElementById("mcontent1").src = 'https://www.openteanews.com/assets/img/disrespectheretics.png'
     document.getElementById("mcontent2").innerHTML = ''
     document.getElementById("mlink1").innerHTML = ''
     document.getElementById("mlink1").href = ''
-    document.getElementById("mtime1").innerHTML = '2/18/23 11:05pm MST'
+    document.getElementById("mtime1").innerHTML = '1/18/24 2:25pm MST'
 
     /* Recent 2 */
-    
+
     document.getElementById("mtitle2").innerHTML = ''
     document.getElementById("mcontent3").src = 'https://www.openteanews.com/assets/img/IMG_9139.png'
     document.getElementById("mcontent4").innerHTML = ''
