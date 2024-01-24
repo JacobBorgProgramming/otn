577c1909503028d32494e37fb5a0d4f9e94b6ed9
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2024-01-23 20:27:10 -0700

    Update verses.js

--- tags/verses.js
+++ tags/verses.js
@@ -1,19 +1,16 @@
-function video(){
+function verse(){
     /* Top most entry */
 
-    document.getElementById("video").src = ''
-    /* rumble: rumble */
-    document.getElementById("video").class = ''
-    /* rumble: 640 */
-    document.getElementById("video").width = ''
-    /* rumble: 360 */
-    document.getElementById("video").height = ''
 
-    document.getElementById("videoalert").innerHTML = 'There are no broadcasts at this time.'
+    document.getElementById("vtitle1").innerHTML = 'Exodus 20:13 NLT'
+    document.getElementById("vcontent1").innerHTML = 'You must not murder.'
+    document.getElementById("vcontent2").innerHTML = ''
+    document.getElementById("vlink1").innerHTML = ''
+    document.getElementById("vlink2").href = ''
+    document.getElementById("vtime2").innerHTML = ''
 /*
 There are no broadcasts at this time.
-Tune in at 00:00 for 
+Tune in at 00:00 for
 */
 
 }
-
